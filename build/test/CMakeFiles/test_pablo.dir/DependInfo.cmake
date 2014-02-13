520b7d7151869682f6a87d84e58a92128dcd14c8
Marco Cisternino <marco.cisternino@optimad.it>
2014-02-13 18:19:36 +0100

    run cmake from build folder "build" with an existing file inside

---
+++ build/test/CMakeFiles/test_pablo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marco/workspace/PABLO/test/test_pablo.cpp" "/home/marco/workspace/PABLO/build/test/CMakeFiles/test_pablo.dir/test_pablo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/marco/workspace/PABLO/build/lib/CMakeFiles/pablo.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
