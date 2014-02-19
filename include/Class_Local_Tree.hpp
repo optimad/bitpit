864375f9d5219f0ab9a45efd5163ddefa5259aef
edoardolombardi <edoardo.lombardi@optimad.it>
2014-02-19 10:33:34 +0100

    Space changed in Local_tree.hpp

--- include/Class_Local_Tree.hpp
+++ include/Class_Local_Tree.hpp
@@ -30,10 +30,10 @@ class Class_Local_Tree{
 	// MEMBERS ----------------------------------------------------------------------- //
 
 private:
-	vector<Class_Octant>  octants;			// Local vector of octants
-	vector<Class_Octant>  ghosts;			// Local vector of ghost octants
-	Class_Octant 		  first_desc;		// First (Zindex order) most refined octant possible in local partition
-	Class_Octant 		  last_desc;		// Last (Zindex order) most refined octant possible in local partition
+	vector<Class_Octant>  octants;			// Local vector of octants ordered with Morton Number
+	vector<Class_Octant>  ghosts;			// Local vector of ghost octants ordered with Morton Number
+	Class_Octant 		  first_desc;		// First (Morton order) most refined octant possible in local partition
+	Class_Octant 		  last_desc;		// Last (Morton order) most refined octant possible in local partition
 	uint32_t 			  size_ghosts;		// Size of vector of ghost octants
 	uint8_t				  local_max_depth;  // Reached max depth in local tree
 
@@ -58,8 +58,8 @@ public:
 	uint8_t       getMarker(int64_t idx);						// Get refinement/coarsening marker for idx-th octant
 	bool          getBalance(int64_t idx);						// Get if balancing-blocked idx-th octant
 
-	void      setMarker(int64_t idx, int8_t marker);			// Set refinement/coarsening marker for idx-th octant
-	void      setBalance(int64_t idx, bool balance);			// Set if balancing-blocked idx-th octant
+	void		  setMarker(int64_t idx, int8_t marker);		// Set refinement/coarsening marker for idx-th octant
+	void		  setBalance(int64_t idx, bool balance);		// Set if balancing-blocked idx-th octant
 
 	//-------------------------------------------------------------------------------- //
 	// Debug methods ----------------------------------------------------------------- //
