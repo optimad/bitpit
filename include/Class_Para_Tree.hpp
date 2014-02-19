072852eab33ca8aeb0a59bcd20eb39fc43a4f90a
edoardolombardi <edoardo.lombardi@optimad.it>
2014-02-19 10:31:12 +0100

    Space OK in comments in in para_tree

--- include/Class_Para_Tree.hpp
+++ include/Class_Para_Tree.hpp
@@ -32,19 +32,19 @@ class Class_Para_Tree {
 	// MEMBERS ----------------------------------------------------------------------- //
 public:
 	//undistributed members
-	uint64_t* partition_range_position; //global array containing position of the last existing octant in each processor
-	uint64_t* partition_range_globalidx; //global array containing global index of the last existing octant in each processor
-	uint64_t global_num_octants; // global number of octants in the parallel octree
+	uint64_t* partition_range_position;		//global array containing position of the last existing octant in each processor
+	uint64_t* partition_range_globalidx;	//global array containing global index of the last existing octant in each processor
+	uint64_t global_num_octants;			// global number of octants in the parallel octree
 	int nproc;
-	uint8_t max_depth; // global max existing level in the parallel octree
+	uint8_t max_depth;						// global max existing level in the parallel octree
 
 	//distributed members
 	int rank;
-	Class_Local_Tree octree; // local tree in each processor
+	Class_Local_Tree octree;				// local tree in each processor
 
 	//auxiliary members
-	int error_flag; // MPI error flag
-	bool serial; // 1 if the octree is the same on each processor, 0 if the octree is distributed
+	int error_flag;							// MPI error flag
+	bool serial;							// 1 if the octree is the same on each processor, 0 if the octree is distributed
 
 
 
