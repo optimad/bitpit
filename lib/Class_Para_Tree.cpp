fa5e0a4e9aa064eef8d477754a7c5dcb837f32e3
Marco Cisternino <marco.cisternino@optimad.it>
2014-02-21 11:48:11 +0100

    fix allgather receive count to nproc

--- lib/Class_Para_Tree.cpp
+++ lib/Class_Para_Tree.cpp
@@ -48,7 +48,7 @@ void Class_Para_Tree::updateLoadBalance() {
 	//update partition_range_globalidx
 	int rbuff [nproc];
 	uint64_t local_num_octants = octree.getNumOctants();
-	error_flag = MPI_Allgather(&local_num_octants,1,MPI_UINT64_T,&rbuff,1,MPI_UINT64_T,MPI_COMM_WORLD);
+	error_flag = MPI_Allgather(&local_num_octants,1,MPI_UINT64_T,&rbuff,nproc,MPI_UINT64_T,MPI_COMM_WORLD);
 	for(int p = 0; p < nproc; ++p){
 		partition_range_globalidx[p] = 0;
 		for(int pp = 0; pp <=p; ++pp)
