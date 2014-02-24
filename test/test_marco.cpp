8f3150ef597db6e8c3434836dfc078565505d7f5
Marco Cisternino <marco.cisternino@optimad.it>
2014-02-24 17:52:16 +0100

    test mpi_pack

--- test/test_marco.cpp
+++ test/test_marco.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 
 	ptree.setPboundGhosts();
 
-	int buff[1000];
+	char buff[1000];
 	int position;
 	int numero = 37;
 	MPI_Pack(&numero, 1, MPI_INT, buff, 1000, &position, MPI_COMM_WORLD);
