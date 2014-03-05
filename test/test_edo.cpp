b83b8b2aaa4aca2616e9f81823d7bf985aa85e6f
edoardolombardi <edoardo.lombardi@optimad.it>
2014-03-05 19:46:53 +0100

    Segmentesciòn folt

--- test/test_edo.cpp
+++ test/test_edo.cpp
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
 
 	Class_Para_Tree ptree;
 
+	ptree.octree.setBalance(0,1);
 	ptree.octree.setMarker(0,1);
 	while(ptree.octree.refine());
 	ptree.updateAdapt();
@@ -38,15 +39,12 @@ int main(int argc, char *argv[]) {
 	if(ptree.rank == 3){
 		ptree.octree.setMarker(3,1);
 	}
-//	ptree.adapt();
-	ptree.octree.refine();
-	ptree.updateAdapt();
+	ptree.adapt();
 
-//	ptree.octree.computeConnectivity();
-//	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"unbalNoGhost");
-//	ptree.octree.computeghostsConnectivity();
-//	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"unbalGhost");
-
+	ptree.octree.computeConnectivity();
+	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"unbalNoGhost");
+	ptree.octree.computeghostsConnectivity();
+	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"unbalGhost");
 
 	ptree.loadBalance();
 	ptree.updateLoadBalance();
@@ -58,17 +56,16 @@ int main(int argc, char *argv[]) {
 	ptree.octree.computeghostsConnectivity();
 	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"balGhost");
 
-	/*
 	if(ptree.rank == 1){
-		for(int i = 0; i < 6; ++i)
+		for(int i = 0; i < ptree.octree.getNumOctants(); ++i)
 			ptree.octree.setMarker(i,-1);
 	}
 	if(ptree.rank == 2){
-		for(int i = 0; i < 2; ++i){
+		for(int i = 0; i < ptree.octree.getNumOctants(); ++i){
 			ptree.octree.setMarker(i,-1);
 		}
 	}
-//	ptree.adapt();
+	ptree.adapt();
 
 	ptree.octree.clearghostsConnectivity();
 	ptree.octree.updateConnectivity();
@@ -85,7 +82,7 @@ int main(int argc, char *argv[]) {
 	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"coarsebalNoGhost");
 	ptree.octree.computeghostsConnectivity();
 	writeLocalTree(ptree.octree.nodes,ptree.octree.connectivity,ptree.octree.ghostsnodes,ptree.octree.ghostsconnectivity,ptree,"coarsebalGhost");
-*/
+
 
 	MPI::Finalize();
 
