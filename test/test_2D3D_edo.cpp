ab3397d3e9926c76e895adc67a087f5941cb7abf
edoardolombardi <edoardo.lombardi@optimad.it>
2014-04-24 00:42:59 +0200

    cmake test_edo

---
+++ test/test_2D3D_edo.cpp
@@ -0,0 +1,27 @@
+/*
+ * test_2D3D.cpp
+ *
+ *  Created on: 14/apr/2014
+ *      Author: Marco Cisternino
+ */
+
+#include "preprocessor_defines.dat"
+#include <mpi.h>
+#include "global.hpp"
+#include "Class_Para_Tree.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	MPI::Init(argc, argv);
+
+	Class_Para_Tree<2> ptree;
+	ptree.octree.setBalance(0,false);
+	ptree.octree.setMarker(0,2);
+	bool done = ptree.adapt();
+
+
+	MPI::Finalize();
+
+}
