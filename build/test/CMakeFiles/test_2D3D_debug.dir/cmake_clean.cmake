6b9ee45256b5427a42c155eca280e7a346201e31
edoardolombardi <edoardo.lombardi@optimad.it>
2014-07-30 15:46:09 +0200

    deleting gh-pages and rebuild

---
+++ build/test/CMakeFiles/test_2D3D_debug.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_2D3D_debug.dir/test_2D3D_debug.cpp.o"
+  "test_2D3D_debug.pdb"
+  "test_2D3D_debug"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_2D3D_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/test/CMakeFiles/test_logo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_logo.dir/test_logo.cpp.o"
+  "test_logo.pdb"
+  "test_logo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_logo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
