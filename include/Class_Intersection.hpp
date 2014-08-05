f7e687b5d6e1b50a43249befeccd3e33d66cdf4f
edoardolombardi <edoardo.lombardi@optimad.it>
2014-08-05 11:17:57 +0200

    inters2

--- include/Class_Intersection.hpp
+++ include/Class_Intersection.hpp
@@ -1,24 +1,6 @@
 #ifndef CLASS_INTERSECTION_HPP_
 #define CLASS_INTERSECTION_HPP_
 
-/*!
- *	\date			23/apr/2014
- *	\authors		Edoardo Lombardi
- *	\authors		Marco Cisternino
- *	\version		0.1
- *
- *	\brief Intersections class definition
- *
- *	Intersections are the faces (edges in 2D) or portion of faces shared by two octants. An intersection is defined
- *	by :
- *	- the owner octants, i.e. the octants sharing the intersection, identified by a couple (array[2]) of indices;
- *	- the index of the face, that contains the intersection, of the first owner;
- *	- an identifier of the octant in the couple with higher level of refinement (0/1) [if same level identifier =0];
- *	- a flag stating if an owner is ghost;
- *	- a flag to communicate if the intersection is new after a mesh refinement.
- *
- */
-
 // =================================================================================== //
 // INCLUDES                                                                            //
 // =================================================================================== //
@@ -38,6 +20,24 @@ using namespace std;
 // =================================================================================== //
 // CLASS DEFINITION                                                                    //
 // =================================================================================== //
+
+/*!
+ *	\date			23/apr/2014
+ *	\authors		Edoardo Lombardi
+ *	\authors		Marco Cisternino
+ *	\version		0.1
+ *
+ *	\brief Intersections class definition
+ *
+ *	Intersections are the faces (edges in 2D) or portion of faces shared by two octants. An intersection is defined
+ *	by :
+ *	- the owner octants, i.e. the octants sharing the intersection, identified by a couple (array[2]) of indices;
+ *	- the index of the face, that contains the intersection, of the first owner;
+ *	- an identifier of the octant in the couple with higher level of refinement (0/1) [if same level identifier =0];
+ *	- a flag stating if an owner is ghost;
+ *	- a flag to communicate if the intersection is new after a mesh refinement.
+ *
+ */
 template<int dim>
 class Class_Intersection{};
 
