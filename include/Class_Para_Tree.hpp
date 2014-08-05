f72cbfc1cb2053385893a3306bf95ea9772f190f
edoardolombardi <edoardo.lombardi@optimad.it>
2014-08-05 11:40:32 +0200

    lc...

--- include/Class_Para_Tree.hpp
+++ include/Class_Para_Tree.hpp
@@ -34,7 +34,7 @@ using namespace std;
  *	\version		0.1
  *	\copyright		Copyright 2014 Optimad engineering srl. All rights reserved.
  *
- *	\license		{PABLO is released under the GNU Public License.}
+ *	\license{PABLO is released under the GNU Public License.}
  *
  *	\brief Parallel Octree Manager Class
  *
