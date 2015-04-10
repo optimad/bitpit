d23be9760f1ff6ebd5c2a4262e3ff561535415c6
edoardo.lombardi <edoardo.lombardi@optimad.it>
2015-04-10 15:05:57 +0200

    Test17 added to script

--- runParallelTests.sh
+++ runParallelTests.sh
@@ -4,6 +4,7 @@ mpiexec -n 4 ./test13;
 mpiexec -n 4 ./test14;
 mpiexec -n 4 ./test15;
 mpiexec -n 4 ./test16;
+mpiexec -n 4 ./test17;
 mpiexec -n 8 ./test116;
 mpiexec -n 8 ./test120;
 mpiexec -n 4 ./testBubbles;
