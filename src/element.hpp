0a2db72f1acb4249a23200e24471c0a0432b3b1c
Andrea Iob <andrea_iob@hotmail.com>
2015-09-04 17:17:14 +0200

    element: add a function to evaluate id hashes
    Since ids are unique, the hasher can be a function that
    takes an id and cast it to a size_t.

--- src/element.hpp
+++ src/element.hpp
@@ -31,6 +31,38 @@ public:
 	    POLYHEDRON
 	};
 
+	/*!
+		Hasher for the ids.
+
+		Since ids are unique, the hasher can be a function that
+		takes an id and cast it to a size_t.
+
+		The hasher is defined as a struct, because a struct can be
+		passed as an object into metafunctions (meaning that the type
+		deduction for the template paramenters can take place, and
+		also meaning that inlining is easier for the compiler). A bare
+		function would have to be passed as a function pointer.
+		To transform a function template into a function pointer,
+		the template would have to be manually instantiated (with a
+		perhaps unknown type argument).
+
+	*/
+	struct IdHasher {
+		/*!
+			Function call operator that casts the specified
+			value to a size_t.
+
+			\tparam U type of the value
+			\param value is the value to be casted
+			\result Returns the value casted to a size_t.
+		*/
+		template<typename U>
+		constexpr std::size_t operator()(U&& value) const noexcept
+		{
+			return static_cast<std::size_t>(std::forward<U>(value));
+		}
+	};
+
 	Element();
 	Element(const int &id);
 
