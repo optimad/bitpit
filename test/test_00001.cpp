6b3f3426cf2067a4185b31de95950dddf9fd4856
Andrea Iob <andrea_iob@hotmail.com>
2015-09-08 12:07:40 +0200

    test: use different names for 2D/3D and Cartesian/octree meshes

--- test/test_00001.cpp
+++ test/test_00001.cpp
@@ -21,14 +21,14 @@ int main(int argc, char *argv[]) {
 	std::cout << "  >> 2D Mesh" << "\n";
 
 	pman::Patch *patch_2D = new pman::PatchCartesian(0, 2, origin, length, dh);
-	patch_2D->set_name("backgorund_mesh");
+	patch_2D->set_name("cartesian_backgorund_mesh_2D");
 	patch_2D->update();
 	patch_2D->output_write();
 
 	std::cout << "  >> 3D Mesh" << "\n";
 
 	pman::Patch *patch_3D = new pman::PatchCartesian(0, 3, origin, length, dh);
-	patch_3D->set_name("backgorund_mesh");
+	patch_3D->set_name("cartesian_backgorund_mesh_3D");
 	patch_3D->update();
 	patch_3D->output_write();
 
--- test/test_00002.cpp
+++ test/test_00002.cpp
@@ -21,14 +21,14 @@ int main(int argc, char *argv[]) {
 	std::cout << "  >> 2D Mesh" << "\n";
 
 	pman::Patch *patch_2D = new pman::PatchOctree(0, 2, origin, length, dh);
-	patch_2D->set_name("backgorund_mesh");
+	patch_2D->set_name("octree_backgorund_mesh_2D");
 	patch_2D->update();
 	patch_2D->output_write();
 
 	std::cout << "  >> 3D Mesh" << "\n";
 
 	pman::Patch *patch_3D = new pman::PatchOctree(0, 3, origin, length, dh);
-	patch_3D->set_name("backgorund_mesh");
+	patch_3D->set_name("octree_backgorund_mesh_3D");
 	patch_3D->update();
 	patch_3D->output_write();
 
