70fb075dc34ae66c697b7b5d0542df574491f5af
Andrea Iob <andrea_iob@hotmail.com>
2015-09-30 11:07:32 +0200

    test: test neighbour search functions

--- test/test_00002.cpp
+++ test/test_00002.cpp
@@ -24,6 +24,37 @@ int main(int argc, char *argv[]) {
 	patch_2D->set_name("octree_backgorund_mesh_2D");
 	patch_2D->update();
 	patch_2D->output_write();
+
+	std::cout << "\n  >> 2D Mesh Neighbour test" << "\n";
+
+	std::vector<int> neighs_2D;
+
+	int cellId_2D = 7;
+	std::cout << std::endl;
+	std::cout << "Cell id: " << cellId_2D << std::endl << std::endl;
+
+	pman::Cell &cell_2D = patch_2D->get_cell(cellId_2D);
+
+	std::cout << "Face neighbours (complete list): " << std::endl;
+	neighs_2D = cell_2D.extract_face_neighs();
+	for (int i = 0; i < neighs_2D.size(); ++i) {
+		std::cout << " - " << neighs_2D[i] << std::endl;
+	}
+
+	std::cout << "Vertex neighbours (complete list): " << std::endl;
+	neighs_2D = cell_2D.extract_vertex_neighs(true);
+	for (int i = 0; i < neighs_2D.size(); ++i) {
+		std::cout << " - " << neighs_2D[i] << std::endl;
+	}
+
+	std::cout << "Vertex neighbours (excuding face neighbours): " << std::endl;
+	neighs_2D = cell_2D.extract_vertex_neighs(false);
+	for (int i = 0; i < neighs_2D.size(); ++i) {
+		std::cout << " - " << neighs_2D[i] << std::endl;
+	}
+
+	std::cout << std::endl;
+
 	delete patch_2D;
 
 	std::cout << "  >> 3D Mesh" << "\n";
@@ -32,6 +63,50 @@ int main(int argc, char *argv[]) {
 	patch_3D->set_name("octree_backgorund_mesh_3D");
 	patch_3D->update();
 	patch_3D->output_write();
+
+
+	std::cout << "\n  >> 3D Mesh Neighbour test" << "\n";
+
+	std::vector<int> neighs_3D;
+
+	int cellId_3D = 13;
+	std::cout << std::endl;
+	std::cout << "Cell id: " << cellId_3D << std::endl << std::endl;
+
+	pman::Cell &cell_3D = patch_3D->get_cell(cellId_3D);
+
+	std::cout << "Face neighbours (complete list): " << std::endl;
+	neighs_3D = cell_3D.extract_face_neighs();
+	for (int i = 0; i < neighs_3D.size(); ++i) {
+		std::cout << " - " << neighs_3D[i] << std::endl;
+	}
+
+	std::cout << "Edge neighbours (complete list): " << std::endl;
+	neighs_3D = cell_3D.extract_edge_neighs(true);
+	for (int i = 0; i < neighs_3D.size(); ++i) {
+		std::cout << " - " << neighs_3D[i] << std::endl;
+	}
+
+	std::cout << "Edge neighbours (excuding face neighbours): " << std::endl;
+	neighs_3D = cell_3D.extract_edge_neighs(false);
+	for (int i = 0; i < neighs_3D.size(); ++i) {
+		std::cout << " - " << neighs_3D[i] << std::endl;
+	}
+
+	std::cout << "Vertex neighbours (complete list): " << std::endl;
+	neighs_3D = cell_3D.extract_vertex_neighs(true);
+	for (int i = 0; i < neighs_3D.size(); ++i) {
+		std::cout << " - " << neighs_3D[i] << std::endl;
+	}
+
+	std::cout << "Vertex neighbours (excuding face and edge neighbours): " << std::endl;
+	neighs_3D = cell_3D.extract_vertex_neighs(false);
+	for (int i = 0; i < neighs_3D.size(); ++i) {
+		std::cout << " - " << neighs_3D[i] << std::endl;
+	}
+
+	std::cout << std::endl;
+
 	delete patch_3D;
 
 #ifndef DISABLE_MPI
