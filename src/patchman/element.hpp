550ef7b57b236b25c09ead72bb5f9c4a9a8253b5
Andrea Iob <andrea_iob@hotmail.com>
2015-11-14 20:26:17 +0100

    patchman/element: make NULL_ELEMENT_ID public

--- src/patchman/element.hpp
+++ src/patchman/element.hpp
@@ -119,9 +119,9 @@ public:
 
 	double eval_min_length() const;
 
-protected:
 	static const long NULL_ELEMENT_ID;
 
+protected:
 	static std::array<double, 3> cross_3D(std::array<double, 3> &x, std::array<double, 3> &y);
 	static void normalize_3D(std::array<double, 3> &x);
 	static void transpose_3D(std::array<std::array<double, 3>, 3> &A);
