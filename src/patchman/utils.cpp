8a5b7f77f163d16779eabc98617e3d0d3dcc496e
Andrea Iob <andrea_iob@hotmail.com>
2015-11-26 17:01:44 +0100

    patchman/utils: add explicit instantiation for add_to_ordered_vector<unsigned long>

--- src/patchman/utils.cpp
+++ src/patchman/utils.cpp
@@ -12,6 +12,7 @@ namespace pman {
 namespace utils {
 
 template bool add_to_ordered_vector<>(const long&, std::vector<long>&, std::less<long>);
+template bool add_to_ordered_vector<>(const unsigned long&, std::vector<unsigned long>&, std::less<unsigned long>);
 
 }
 
