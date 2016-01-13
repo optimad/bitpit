2e9c8aa99363cfd880e00af3d2faeb4e9dc457de
Haysam Telib <haysam.telib@optimad.it>
2016-01-13 12:38:38 +0100

    FileHandler_C: add doxygen documentation

--- src/IO/Class_FH.cpp
+++ src/IO/Class_FH.cpp
@@ -1,6 +1,16 @@
-#include"Class_FH.hpp"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
 
-//---------------------------------------------------------------
+#include"Class_FH.hpp"
+#include"Operators.hpp"
+
+/*! ---------------------------------------------------------------
+ * Default constructor. 
+ * Refers to a serial file in the current directory called "file.dat"
+ * Default values of counters and blocks are set to 0.
+ */
 FileHandler_C::FileHandler_C(){
    
     directory  = "./" ; 
@@ -8,37 +18,47 @@ FileHandler_C::FileHandler_C(){
     appendix   = "dat" ;
     series     = false ;
     parallel   = false ;
-    counter    = 999 ;
-    block      = 999 ;
+    counter    = 0 ;
+    block      = 0 ;
     
 } ;
 
-//---------------------------------------------------------------
-FileHandler_C::FileHandler_C( string dir_, string name_, string app_):
+/*! ---------------------------------------------------------------
+ * Constructor. 
+ * Refers to a serial file named according to input parameters.
+ * Default values of counters and blocks are set to 0.
+ * \param[in]   dir_    directory of file
+ * \param[in]   name_   name of the file
+ * \param[in]   app_    appendix of the file
+ */
+FileHandler_C::FileHandler_C( std::string dir_, std::string name_, std::string app_):
     directory(dir_), name(name_), appendix(app_){
     series     = false ;
     parallel   = false ;
-    counter    = 999 ;
-    block      = 999 ;
+    counter    = 0 ;
+    block      = 0 ;
 };
 
-//---------------------------------------------------------------
+/*! ---------------------------------------------------------------
+ * Copy Constructor. 
+ * \param[in]   other    FileHandler_C to be copied.
+ */
 FileHandler_C::FileHandler_C(const FileHandler_C& other){
 
     (*this) = other ;
 
 };
 
-//---------------------------------------------------------------
+/*! ---------------------------------------------------------------
+ * Destructor.
+ */
 FileHandler_C::~FileHandler_C(){
 
-    if (str.is_open()) {
-        str.close() ;
-        
-    };
 } ;
 
-//---------------------------------------------------------------
+/*! ---------------------------------------------------------------
+ * Assignment operator.
+ */
 FileHandler_C&    FileHandler_C::operator=(const FileHandler_C& other){
 
     directory   = other.directory ;
@@ -49,38 +69,87 @@ FileHandler_C&    FileHandler_C::operator=(const FileHandler_C& other){
     counter     = other.counter ;
     block       = other.block ;
 
-    str.clear() ;
 
     return *this ;
 };
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetDirectory( string d_){ directory=d_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets the directory
+ * \param[in]   d_      name of directory
+ */
+void    FileHandler_C::SetDirectory( std::string d_){ 
+    directory=d_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetName( string n_){ name=n_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets the name
+ * \param[in]   n_      name of file
+ */
+void    FileHandler_C::SetName( std::string n_){ 
+    name=n_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetAppendix( string a_){ appendix=a_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets the appendix
+ * \param[in]   a_      appendix of file
+ */
+void    FileHandler_C::SetAppendix( std::string a_){ 
+    appendix=a_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetSeries( bool s_){ series=s_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets if file belongs to a time series
+ * \param[in]   s_      [true/false] if series
+ */
+void    FileHandler_C::SetSeries( bool s_){ 
+    series=s_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetParallel( bool p_){ parallel=p_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets the counter of the series
+ * \param[in]   c_      index of series
+ */
+void    FileHandler_C::SetCounter(int c_){ 
+    counter=c_; 
+    return; 
+};
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetCounter(int c_){ counter=c_; return; };
+/*! ---------------------------------------------------------------
+ * Sets if file belongs to a parallel output
+ * \param[in]   s_      [true/false] if parallel
+ */
+void    FileHandler_C::SetParallel( bool p_){ 
+    parallel=p_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::SetBlock( int b_){ block=b_; return;} ;
+/*! ---------------------------------------------------------------
+ * Sets the counter of the series
+ * \param[in]   v_      index of block of parallel set
+ */
+void    FileHandler_C::SetBlock( int b_){ 
+    block=b_; 
+    return;
+} ;
 
-//---------------------------------------------------------------
-void    FileHandler_C::IncrementCounter(){ if(series) counter++; return; };
+/*! ---------------------------------------------------------------
+ * Increments the counter used for series.
+ */
+void    FileHandler_C::IncrementCounter(){ 
+    counter++; 
+    return; 
+};
 
-//---------------------------------------------------------------
-string  FileHandler_C::GetName(){
-  stringstream filename ;
+/*! ---------------------------------------------------------------
+ * Composes the filename.
+ * \return  complete filename
+ */
+std::string  FileHandler_C::GetName(){
+  std::stringstream filename ;
 
   filename << directory << "/"<<name ;
   if(parallel) filename <<".b"<< ZeroPadNumber(4, block)    ;
@@ -90,8 +159,12 @@ string  FileHandler_C::GetName(){
   return filename.str() ;
 
 };
-//---------------------------------------------------------------
+
+/*! ---------------------------------------------------------------
+ * Checks if file exists.
+ * \return  [true/false] if file exists
+ */
 bool   FileHandler_C::Exists() {
-    ifstream f( GetName() );
+    std::ifstream f( GetName() );
     return f.good() ;
 };
--- src/IO/Class_FH.hpp
+++ src/IO/Class_FH.hpp
@@ -6,45 +6,50 @@
 #include <fstream>
 #include <sstream>
 
-#include "Operators.hpp"
 
-using namespace std;
-
+/*!
+ * \class   FileHandler_C
+ * \brief   Creates file names and checks status
+ *
+ * Permits to create easily filenames for parallel output and for writing time series.
+ * Typical filenames are *directory/name.bxxxx.yyyy.appendix* and can be retirieved via GetName().
+ * - *directory* set via SetDirectory( std::string)
+ * - *name* set via SetName(std::string)
+ * - *bxxxx* is added if SetParallel(true) and SetBlock(xxxx) have been called
+ * - *yyyy* is added if SetSeries(true) and SetCounter(yyyy) have been called. yyyy is incremented by one if IncrementCounter() is called.
+ */
 class FileHandler_C{
 
-    public:
-    fstream               str;
-
-    protected:
-    string                directory;       // name od directory where file resides
-    string                name;            // name of file;
-    string                appendix ;       // appendix;
-    bool                  series ;         // is time series?
-    bool                  parallel ;       // is part of distributed data?
-    int                   counter ;        // counter for time series ;
-    int                   block  ;         // index of block if parallel data
+    private:
+        std::string           directory;       /**< name od directory where file resides */
+        std::string           name;            /**< name of file; */
+        std::string           appendix ;       /**< appendix; */
+        bool                  series ;         /**< is time series? */
+        bool                  parallel ;       /**< is part of distributed data? */
+        int                   counter ;        /**< counter for time series ; */
+        int                   block  ;         /**< index of block if parallel data */
 
 
     public:
-    FileHandler_C() ;
-    FileHandler_C( string dir_, string name_, string app_) ;
-    FileHandler_C(  const FileHandler_C& other ) ;
+        FileHandler_C() ;
+        FileHandler_C( std::string dir_, std::string name_, std::string app_) ;
+        FileHandler_C(  const FileHandler_C& other ) ;
 
-    ~FileHandler_C() ;
-    
-    FileHandler_C& operator=( const FileHandler_C& other) ;
+        ~FileHandler_C() ;
 
-    void                SetDirectory( string d_) ;
-    void                SetName( string n_) ;
-    void                SetAppendix( string a_) ;
-    void                SetSeries( bool s_) ;
-    void                SetParallel( bool p_) ;
-    void                SetCounter(int c_);
-    void                SetBlock( int b_) ;
+        FileHandler_C& operator=( const FileHandler_C& other) ;
 
-    void                IncrementCounter();
-    string              GetName() ;
-    bool                Exists() ;
+        void                SetDirectory( std::string d_) ;
+        void                SetName( std::string n_) ;
+        void                SetAppendix( std::string a_) ;
+        void                SetSeries( bool s_) ;
+        void                SetParallel( bool p_) ;
+        void                SetCounter(int c_);
+        void                SetBlock( int b_) ;
+
+        void                IncrementCounter();
+        std::string         GetName() ;
+        bool                Exists() ;
 
 };
 
