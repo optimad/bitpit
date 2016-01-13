16dd44c4f5e967006e82dd6578db256ee171f9ad
edoardolombardi <edoardo.lombardi@optimad.it>
2016-01-13 15:35:51 +0100

    PABLO old version backup removed

--- src/pablo/old_version/Class_Local_Tree.cpp
+++
@@ -1,9 +0,0 @@
-#include "Class_Local_Tree.hpp"
-
-// =================================================================================== //
-// EXPLICIT INSTANTIATION                                                              //
-// =================================================================================== //
-
-template<> class Class_Local_Tree<2>;
-template<> class Class_Local_Tree<3>;
-
