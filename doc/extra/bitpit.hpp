83473fc851bc14024d23da71ea20cd4df3cb1cf6
Haysam Telib <haysam.telib@optimad.it>
2016-01-29 14:11:14 +0100

    documentation: add bitpit mainpage

--- doc/extra/bitpit.hpp
+++ doc/extra/bitpit.hpp
@@ -2,8 +2,48 @@
 
 \mainpage bitpit
 
-<B>bitpit</B>.
+<B>bitpit</B> is a C++ library for scientific high-performance computing.
+<B>bitpit</B> is composed of several modules:
 
-@tableofcontents
+Basic modules
+========================
+
+1. common
+------------------------
+<B>common</B> provides generic functions for the bitpit framework
+
+2. operators
+------------------------
+- <B> + - * / </B> operators for the std::vector and std::array classes
+- basic mathematic functions like <B> dotProduct crossProduct norm abs ...</B>  for the std::vector and std::array classes
+- <B> << >> </B> operators and <B> display </B> functions for the std::vector and std::array classes
+- basic functions to manipulate std::strings
+
+3. containers
+------------------------
+- <B>PiercedVector</B> is a container which allows the cancellation and insertion on the fly of elements.
+- <B>CollapsedArray2D & CollapsedVector2D</B> are 2D linearized vectors which avoid the overhead of the vector infrastructures.
+- <B>I/O BinaryStream</B> allow to copy chunks of memory
+
+4. Input Output (IO)
+------------------------
+- <B>GenericIO</B> basic routines to read/write in ASCII or BINARY
+- <B>DGF</B> classes and routines for reading and writing Dune Grid Format files
+- <B>STL</B> classes and routines for reading and writing Stereo Litography files
+- <B>VTK</B> classes and routines for reading and writing Visualization ToolKit files
+
+5. Linear Algebra (LA)
+------------------------
+<B>LA</B> providesi methods for small dense linear systems stored as std::vector<std::vector> or std::array<std::array>
+- creation of basic matrices, like identity and diagonal matrices
+- basic matrix operations like transpose and matrix multiplication
+- solution 
+
+
+6. Sort algorithms (SA)
+------------------------
+- <B>LIFOStack</B> manage insertion and extraction uding the Last-In-First-Out pragma
+- <B>KdTree</B> sorts vertices in a d-dimensional Euclidean space in a Kd-tree structure
+- <B>MinPQueue & MaxPQueue</B> manage a binary tree which ensures that the root element has the smalles/largest value in the tree
 
 */
