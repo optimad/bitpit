aa5eb5eff1534639353192a013e3001136225a7c
Andrea Iob <andrea_iob@hotmail.com>
2016-01-29 08:30:54 +0100

    bitpit: fixes to the license header

--- src/common/utils.hpp
+++ src/common/utils.hpp
@@ -14,13 +14,13 @@
  *
  *  BitPit is distributed in the hope that it will be useful, but WITHOUT
  *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
  *
- \*---------------------------------------------------------------------------*/
+\*---------------------------------------------------------------------------*/
 
 #ifndef __BITPIT_UTILS_HPP__
 #define __BITPIT_UTILS_HPP__
--- src/operators/Operators.tpp
+++ src/operators/Operators.tpp
@@ -14,8 +14,8 @@
  *
  *  BitPit is distributed in the hope that it will be useful, but WITHOUT
  *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *  for more details.
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
