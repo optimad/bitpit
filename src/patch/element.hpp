e5b6e8b11f30d79204120284902c03ba3bdd4982
Andrea Iob <andrea_iob@hotmail.com>
2016-02-03 18:25:21 +0100

    patch: add a license header to patch files

--- src/patch/element.hpp
+++ src/patch/element.hpp
@@ -1,6 +1,27 @@
-//
-// Written by Andrea Iob <andrea_iob@hotmail.com>
-//
+/*---------------------------------------------------------------------------*\
+ *
+ *  bitpit
+ *
+ *  Copyright (C) 2015-2016 OPTIMAD engineering Srl
+ *
+ *  -------------------------------------------------------------------------
+ *  License
+ *  This file is part of bitbit.
+ *
+ *  bitpit is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License v3 (LGPL)
+ *  as published by the Free Software Foundation.
+ *
+ *  bitpit is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
+ *
+\*---------------------------------------------------------------------------*/
+
 #ifndef __BITP_MESH_ELEMENT_HPP__
 #define __BITP_MESH_ELEMENT_HPP__
 
