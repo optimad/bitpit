dc754d02bf91b4ade652ae30b551bb3d4c3dfaa4
Andrea Iob <andrea_iob@hotmail.com>
2016-02-12 08:59:46 +0100

    voltripatch: add an empty implementation of the module 'voltripatch'
    The VolTriPatch class will be used to define unstructured volume
    triangulations.

---
+++ src/voltripatch/voltripatch.cpp
@@ -0,0 +1,235 @@
+/*---------------------------------------------------------------------------*\
+ *
+ *  bitpit
+ *
+ *  Copyright (C) 2015-2016 OPTIMAD engineering Srl
+ *
+ *  -------------------------------------------------------------------------
+ *  License
+ *  This file is part of bitbit.
+ *
+ *  bitpit is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License v3 (LGPL)
+ *  as published by the Free Software Foundation.
+ *
+ *  bitpit is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
+ *
+\*---------------------------------------------------------------------------*/
+
+#include "bitpit_common.hpp"
+
+#include "voltripatch.hpp"
+
+namespace bitpit {
+
+/*!
+	\ingroup voltripatch
+	@{
+*/
+
+/*!
+	\class VolTriPatch
+
+	\brief The VolTriPatch class defines an unstructured volume
+	triangulation.
+
+	VolTriPatch defines an unstructured volume triangulation.
+*/
+
+/*!
+	Creates a new patch.
+
+	\param id is the id of the patch
+*/
+VolTriPatch::VolTriPatch(const int &id)
+	: Patch(id, 2)
+{
+
+}
+
+/*!
+	Destroys the patch.
+*/
+VolTriPatch::~VolTriPatch()
+{
+}
+
+/*!
+ * Enables or disables expert mode.
+ *
+ * When expert mode is enabled, it will be possible to change the
+ * patch using low level functions (e.g., it will be possible to
+ * add individual cells, add vertices, delete cells, ...).
+ *
+ * \param expert if true, the expert mode will be enabled
+ */
+void VolTriPatch::setExpert(bool expert)
+{
+	Patch::setExpert(expert);
+}
+
+/*!
+	Evaluates the volume of the specified cell.
+
+	\param id is the id of the cell
+	\result The volume of the specified cell.
+*/
+double VolTriPatch::evalCellVolume(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return 0;
+}
+
+/*!
+	Evaluates the characteristic size of the specified cell.
+
+	\param id is the id of the cell
+	\result The characteristic size of the specified cell.
+*/
+double VolTriPatch::evalCellSize(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return 0;
+}
+
+/*!
+	Evaluates the area of the specified interface.
+
+	\param id is the id of the interface
+	\result The area of the specified interface.
+*/
+double VolTriPatch::evalInterfaceArea(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return 0;
+}
+
+/*!
+	Evaluates the normal of the specified interface.
+
+	\param id is the id of the interface
+	\result The normal of the specified interface.
+*/
+std::array<double, 3> VolTriPatch::evalInterfaceNormal(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return {{0., 0., 0.}};
+}
+
+/*!
+	Updates the patch.
+
+	\result Returns a vector of Adaption::Info that can be used to track
+	the changes done during the update.
+*/
+const std::vector<Adaption::Info> VolTriPatch::_update(bool trackAdaption)
+{
+	if (!isDirty()) {
+		return std::vector<Adaption::Info>();
+	}
+
+	std::cout << ">> Updating surface triangulation mesh\n";
+
+	// Adaption info
+	std::vector<Adaption::Info> adaptionData;
+	if (trackAdaption) {
+
+	}
+
+	// Done
+	return adaptionData;
+}
+
+/*!
+	Marks a cell for refinement.
+
+	This is a void function since mesh refinement is not implemented
+	for SurfTri patches.
+
+	\param id is the id of the cell that needs to be refined
+*/
+bool VolTriPatch::_markCellForRefinement(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return false;
+}
+
+/*!
+	Marks a cell for coarsening.
+
+	This is a void function since mesh refinement is not implemented
+	for SurfTri patches.
+
+	\param id the cell to be refined
+*/
+bool VolTriPatch::_markCellForCoarsening(const long &id)
+{
+	BITPIT_UNUSED(id);
+
+	return false;
+}
+
+/*!
+	Enables cell balancing.
+
+	This is a void function since mesh refinement is not implemented
+	for SurfTri patches.
+
+	\param id is the id of the cell
+	\param enabled defines if enable the balancing for the specified cell
+*/
+bool VolTriPatch::_enableCellBalancing(const long &id, bool enabled)
+{
+	BITPIT_UNUSED(id);
+	BITPIT_UNUSED(enabled);
+
+	return false;
+}
+
+/*!
+ * Checks if the specified point is inside the patch.
+ *
+ * \param[in] point is the point to be checked
+ * \result Returns true if the point is inside the patch, false otherwise.
+ */
+bool VolTriPatch::isPointInside(const std::array<double, 3> &point)
+{
+	BITPIT_UNUSED(point);
+
+	return false;
+}
+
+/*!
+ * Locates the cell the contains the point.
+ *
+ * If the point is not inside the patch, the function returns the id of the
+ * null element.
+ *
+ * \param[in] point is the point to be checked
+ * \result Returns the linear id of the cell the contains the point. If the
+ * point is not inside the patch, the function returns the id of the null
+ * element.
+ */
+long VolTriPatch::locatePoint(const std::array<double, 3> &point)
+{
+	BITPIT_UNUSED(point);
+
+	return false;
+}
+
+/*!
+	@}
+*/
+
+}
