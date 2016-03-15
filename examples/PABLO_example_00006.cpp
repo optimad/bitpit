224da8145e194d114668e1381b42eb10c1b8a7ee
Marco Cisternino <marco.cisternino@optimad.it>
2016-03-15 16:41:45 +0100

    PABLO: add code snippets to example 6 and 7 documentation

--- examples/PABLO_example_00006.cpp
+++ examples/PABLO_example_00006.cpp
@@ -33,44 +33,86 @@ using namespace bitpit;
 
 // =================================================================================== //
 /*!
-	\example PABLO_example_00006.cpp
+  \example PABLO_example_00006.cpp
+  
+  Parallel 2D smoothing data using PABLO.
+  
+  
+  The example is the parallel version of PABLO_example_00003.
+  
+  In order to perform the smoothing procedure in parallel, ghost elements and data communications towards them are needed. Now ghost quadrants and ghost data are not only instantiated, as in example 00003, but actually used to perform smoothing across processor borders.
+  
+  The user data communication interfaces are based on the Couriously Recurrent Template Pattern. The user has to implement a specification of the interface by writing a derived class.
+  In the files PABLO_userDataComm.hpp and PABLO_userDataComm.tpp an example of this specification is given in the case of user data stored in a POD container similar to the STL vector.
+  
+  The class in PABLO_userDataComm.hpp is an example of user specification of the data communication interface based on the Curiously Recurrent Template Pattern.
+  The user has to implement his interface class(es) in order to define how his data have to be written and read in the communication buffer.
+  These classes have to be derived from the template base class bitpit::DataCommInterface using as template argument value the derived class. Like this,
+  @code
+  template <class D>
+  class UserDataComm : public bitpit::DataCommInterface< UserDataComm<D> > {
+  @endcode
 
-	Parallel 2D smoothing data using PABLO.
+  The choice of the members of the class is completely up to the user and they have to be useful to access both internal and ghost data container. In the example user data datatype is given as template parameter in order to pass any container similar to the STL vector.
+  
+  In any case, the user must at least implement all the methods reported in this example:
+  - <b><code>size_t fixedsize()</code> method:</b> this method is automatically called by the manager and it is intended to define the constant size of the data to be communicated per grid element. If all the element of the grid communicate the same size of data, this method must return a value different from zero and equal to the number of byte to be communicated for every element. 	Otherwise, it must return zero and the different data size for each element must be specified by the size method
+  @code
+  template<class Data>
+  inline size_t UserDataComm<Data>::fixedSize() const {
+  return 0;
+  };
+  @endcode
+  \return the size in bytes of the data tobe communicated for every element
 
+  - <b><code>size_t size(const uint32_t e)</code> method:</b> this method is automatically called by the manager and it is intended to define the variable size of the data to be communicated of every grid element. In order to make the manager use this method, the fixedsize method has to return zero. Implementing this method, the user can pass the manager the specific data size to be communicated for the element e.
+  @code
+  template<class Data>
+  inline size_t UserDataComm<Data>::size(const uint32_t e) const {
+    return sizeof(double);
+  };
+  @endcode
+  \param[in] index of the internal element to be communicated.
+  \return the size in bytes of the data tobe communicated for the element e
 
-	The example is the parallel version of PABLO_example_00003.
+  - <b><code>void gather(Buffer& buff, const uint32_t e)</code> method: </b> this method is automatically called by the manager and it is intended to write user data to the char communication buffer. The user has to specify in its implementation the way data can be written in the char buffer. The manager provide the user with a buffer and its write method in order to simply write POD data in the buffer.	The user has to define the way his data can be written in the buffer by decomposing them in POD data and by using the buffer write method to store them in the buffer. In this example we suppose that data is a container of POD data having the random access operator.
+  @code
+  template<class Data>
+  template<class Buffer>
+  inline void UserDataComm<Data>::gather(Buffer& buff, const uint32_t e) {
+    buff.write(data[e]);
+  };
+  @endcode
+  \param[in] e index of the internal element to be communicated.
+  \param[in] buff is the char buffer used to communicate user data. The user has not to take care of the buffer, but its method write and read. These methods are intended to write/read POD data to/fromthe buffer.
 
-	In order to perform the smoothing procedure in parallel, ghost elements and data communications towards them are needed. Now ghost quadrants and ghost data are not only instantiated, as in example 00003, but actually used to perform smoothing across processor borders.
+  - <b><code>void scatter(Buffer& buff, const uint32_t e)</code> method: </b> this method is automatically called by the manager and it is intended to read user data from the char communication buffer and store them in the ghost data container. The user has to specify in its implementation the way data can be read from the char buffer.	The manager provide the user with a buffer and its read method in order to simply read POD data from the buffer. The user has to define the way his data can be read from the buffer by decomposing them in POD data and by using the buffer read method to take them from the buffer and to store them in the ghost data container. In this example we suppose that ghostData is a container of POD data having the random access operator.
+  @code
+  template<class Data>
+  template<class Buffer>
+  inline void UserDataComm<Data>::scatter(Buffer& buff,	const uint32_t e) {
+    buff.read(ghostData[e]);
+  };
+  @endcode
+  \param[in] e index of the ghost element to be communicated.
+  \param[in] buff is the char buffer used to communicate user data. The user has not to take care of the buffer, but its method write and read. These methods are intended to write/read POD data to/fromthe buffer.
 
-	The user data communication interfaces are based on the Couriously Recurrent Template Pattern. The user has to implement a specification of the interface by writing a derived class.
-	In the files PABLO_userDataComm.hpp and PABLO_userDataComm.tpp an example of this specification is given in the case of user data stored in a POD container similar to the STL vector.
-
-	The class in PABLO_userDataComm.hpp is an example of user specification of the data communication interface based on the Curiously Recurrent Template Pattern.
-	The user has to implement his interface class(es) in order to define how his data have to be written and read in the communication buffer.
-	These classes have to be derived from the template base class bitpit::DataCommInterface using as template argument value the derived class.
-
-	The choice of the members of the class is completely up to the user and they have to be useful to access both internal and ghost data container. In the example user data datatype is given as template parameter in order to pass any container similar to the STL vector.
-
-	In any case, the user must at least implement all the methods reported in this example:
-	- <b><code>size_t fixedsize()</code> method:</b> this method is automatically called by the manager and it is intended to define the constant size of the data to be communicated per grid element. If all the element of the grid communicate the same size of data, this method must return a value different from zero and equal to the number of byte to be communicated for every element. 	Otherwise, it must return zero and the different data size for each element must be specified by the size method	
-	\return the size in bytes of the data tobe communicated for every element
-
-	- <b><code>size_t size(const uint32_t e)</code> method:</b> this method is automatically called by the manager and it is intended to define the variable size of the data to be communicated of every grid element. In order to make the manager use this method, the fixedsize method has to return zero. Implementing this method, the user can pass to the manager the specific data size to be communicated for the element e.
-	\param[in] index of the internal element to be communicated.
-	\return the size in bytes of the data tobe communicated for the element e
-
-	- <b><code>void gather(Buffer& buff, const uint32_t e)</code> method: </b> this method is automatically called by the manager and it is intended to write user data to the char communication buffer. The user has to specify in its implementation the way data can be written in the char buffer. The manager provide the user with a buffer and its write method in order to simply write POD data in the buffer.	The user has to define the way his data can be written in the buffer by decomposing them in POD data and by using the buffer write method to store them in the buffer. In this example we suppose that data is a container of POD data having the random access operator.
-	\param[in] e index of the internal element to be communicated.
-	\param[in] buff is the char buffer used to communicate user data. The user has not to take care of the buffer, but its method write and read. These methods are intended to write/read POD data to/fromthe buffer.
-
-	- <b><code>void scatter(Buffer& buff, const uint32_t e)</code> method: </b> this method is automatically called by the manager and it is intended to read user data from the char communication buffer and store them in the ghost data container. The user has to specify in its implementation the way data can be read from the char buffer.	The manager provide the user with a buffer and its read method in order to simply read POD data from the buffer. The user has to define the way his data can be read from the buffer by decomposing them in POD data and by using the buffer read method to take them from the buffer and to store them in the ghost data container. In this example we suppose that ghostData is a container of POD data having the random access operator.
-	\param[in] e index of the ghost element to be communicated.
-	\param[in] buff is the char buffer used to communicate user data. The user has not to take care of the buffer, but its method write and read. These methods are intended to write/read POD data to/fromthe buffer.
-
-	<b>To run</b>: ./PABLO_example_00006 \n
-
-	<b>To see the result visit</b>: <a href="http://optimad.github.io/PABLO/">PABLO website</a> \n
-
+  - <b><code>UserDataComm(Data& data_, Data& ghostdata_)</code> the constructor method: </b> this method has to be called by the user in his application code. The user is free to implement his constructors as he wants, but he must guarantee the access to the internal and ghost data.
+  @code 
+  template<class D>
+  inline UserDataComm<D>::UserDataComm(Data& data_, Data& ghostdata_) : data(data_), ghostdata(ghostdata_){}
+  @endcode
+  
+  In the code of this example application, pay attention to the use of the interface
+  @code
+  UserDataLB<vector<double> > data_lb(weight,weightGhost);
+  pablo7.loadBalance(data_lb, &weight);
+  @endcode
+  
+  <b>To run</b>: ./PABLO_example_00006 \n
+  
+  <b>To see the result visit</b>: <a href="http://optimad.github.io/PABLO/">PABLO website</a> \n
+  
 */
 // =================================================================================== //
 
