635ac55c8d957a7335546d84f9654afa21093d7d
Andrea Iob <andrea_iob@hotmail.com>
2016-03-16 10:31:31 +0100

    test/IO: remove C++ MPI bindings

--- test/IO/test_IO_00002.cpp
+++ test/IO/test_IO_00002.cpp
@@ -35,7 +35,7 @@ using namespace bitpit;
 int main(int argc, char *argv[]) {
 
 #if BITPIT_ENABLE_MPI==1
-	MPI::Init(argc,argv);
+	MPI_Init(&argc, &argv);
 #else
 	BITPIT_UNUSED(argc);
 	BITPIT_UNUSED(argv);
@@ -197,7 +197,7 @@ int main(int argc, char *argv[]) {
 	log::cout() << "<------------------------->" << "\n";
 
 #if BITPIT_ENABLE_MPI==1
-	MPI::Finalize();
+	MPI_Finalize();
 #endif
 
 }
