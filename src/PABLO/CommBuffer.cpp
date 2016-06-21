c407331ef15cf36da0ef958c9f679106a522d93c
Marco Cisternino <marco.cisternino@optimad.it>
2016-06-21 12:01:42 +0200

    PABLO: correct indentation

--- src/PABLO/CommBuffer.cpp
+++ src/PABLO/CommBuffer.cpp
@@ -20,7 +20,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
  *
-\*---------------------------------------------------------------------------*/
+ \*---------------------------------------------------------------------------*/
 
 #if BITPIT_ENABLE_MPI==1
 
@@ -33,71 +33,71 @@
 
 namespace bitpit {
 
-// =================================================================================== //
-// NAME SPACES                                                                         //
-// =================================================================================== //
-using namespace std;
+    // =================================================================================== //
+    // NAME SPACES                                                                         //
+    // =================================================================================== //
+    using namespace std;
 
 
-CommBuffer::CommBuffer(){
+    CommBuffer::CommBuffer(){
 
-	m_commBufferSize = 0;
-	m_commBuffer = NULL;
-	m_pos = 0;
-	m_comm = MPI_COMM_WORLD;
-}
+        m_commBufferSize = 0;
+        m_commBuffer = NULL;
+        m_pos = 0;
+        m_comm = MPI_COMM_WORLD;
+    }
 
-CommBuffer::CommBuffer(MPI_Comm comm_) : m_comm(comm_){
+    CommBuffer::CommBuffer(MPI_Comm comm_) : m_comm(comm_){
 
-	m_commBufferSize = 0;
-	m_commBuffer = NULL;
-	m_pos = 0;
-}
+        m_commBufferSize = 0;
+        m_commBuffer = NULL;
+        m_pos = 0;
+    }
 
-CommBuffer::CommBuffer(uint32_t size, char value, MPI_Comm comm_) : m_comm(comm_){
+    CommBuffer::CommBuffer(uint32_t size, char value, MPI_Comm comm_) : m_comm(comm_){
 
-	m_commBufferSize = size;
-	m_commBuffer = new char [size];
-	for(uint32_t i = 0; i < size; ++i)
-		m_commBuffer[i] = value;
-	m_pos = 0;
-}
+        m_commBufferSize = size;
+        m_commBuffer = new char [size];
+        for(uint32_t i = 0; i < size; ++i)
+            m_commBuffer[i] = value;
+        m_pos = 0;
+    }
 
-CommBuffer::CommBuffer(const CommBuffer& other) {
+    CommBuffer::CommBuffer(const CommBuffer& other) {
 
-	m_commBufferSize = other.m_commBufferSize;
-	//	if(commBuffer != NULL){
-	//		delete [] commBuffer;
-	//		commBuffer = NULL;
-	//	}
-	m_commBuffer = new char [m_commBufferSize];
-	for(uint32_t i = 0; i < m_commBufferSize; ++i)
-		m_commBuffer[i] = other.m_commBuffer[i];
-	m_pos = other.m_pos;
-	m_comm = other.m_comm;
-}
+        m_commBufferSize = other.m_commBufferSize;
+        //	if(commBuffer != NULL){
+        //		delete [] commBuffer;
+        //		commBuffer = NULL;
+        //	}
+        m_commBuffer = new char [m_commBufferSize];
+        for(uint32_t i = 0; i < m_commBufferSize; ++i)
+            m_commBuffer[i] = other.m_commBuffer[i];
+        m_pos = other.m_pos;
+        m_comm = other.m_comm;
+    }
 
-CommBuffer::~CommBuffer() {
+    CommBuffer::~CommBuffer() {
 		delete [] m_commBuffer;
 		m_commBuffer = NULL;
-}
+    }
 
-CommBuffer& CommBuffer::operator =(const CommBuffer& rhs) {
-	if(this != &rhs)
-	{
-		char* new_array = new char[rhs.m_commBufferSize];
-		copy(rhs.m_commBuffer,rhs.m_commBuffer+rhs.m_commBufferSize,new_array);
+    CommBuffer& CommBuffer::operator =(const CommBuffer& rhs) {
+        if(this != &rhs)
+            {
+                char* new_array = new char[rhs.m_commBufferSize];
+                copy(rhs.m_commBuffer,rhs.m_commBuffer+rhs.m_commBufferSize,new_array);
 
-		delete [] m_commBuffer;
+                delete [] m_commBuffer;
 
-		m_commBuffer = new_array;
-		m_commBufferSize = rhs.m_commBufferSize;
-		m_pos = rhs.m_pos;
-		m_comm = rhs.m_comm;
-	}
-	return *this;
+                m_commBuffer = new_array;
+                m_commBufferSize = rhs.m_commBufferSize;
+                m_pos = rhs.m_pos;
+                m_comm = rhs.m_comm;
+            }
+        return *this;
 
-}
+    }
 
 }
 
--- src/PABLO/CommBuffer.hpp
+++ src/PABLO/CommBuffer.hpp
@@ -20,7 +20,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
  *
-\*---------------------------------------------------------------------------*/
+ \*---------------------------------------------------------------------------*/
 
 #if BITPIT_ENABLE_MPI==1
 #ifndef __BITPIT_COMMBUFFER_HPP__
@@ -35,85 +35,85 @@
 
 namespace bitpit {
 
-// =================================================================================== //
-// CLASS DEFINITION                                                                    //
-// =================================================================================== //
-/*!
- * \ingroup PABLO
- * @{
- *
- *	\date			09/sep/2015
- *	\authors		Edoardo Lombardi
- *	\authors		Marco Cisternino
- *	\copyright		Copyright 2014 Optimad engineering srl. All rights reserved.
- *	\par			License
- *	This version of PABLO is released under the LGPL License.
- *
- *	\brief Bundle char container for communications
- *
- *	This calls is intended to provide the user with a basic container
- *	for data MPI communications.
- *
- *	The user should use this container implementing his communications
- *	interface specializations.
- *
- *	More precisely, he has to call read/write methods to read/write
- *	every MPI-compatible POD datum in the buffer.
- *	By this way, data communications are data independent.
- */
-class CommBuffer {
+    // =================================================================================== //
+    // CLASS DEFINITION                                                                    //
+    // =================================================================================== //
+    /*!
+     * \ingroup PABLO
+     * @{
+     *
+     *	\date			09/sep/2015
+     *	\authors		Edoardo Lombardi
+     *	\authors		Marco Cisternino
+     *	\copyright		Copyright 2014 Optimad engineering srl. All rights reserved.
+     *	\par			License
+     *	This version of PABLO is released under the LGPL License.
+     *
+     *	\brief Bundle char container for communications
+     *
+     *	This calls is intended to provide the user with a basic container
+     *	for data MPI communications.
+     *
+     *	The user should use this container implementing his communications
+     *	interface specializations.
+     *
+     *	More precisely, he has to call read/write methods to read/write
+     *	every MPI-compatible POD datum in the buffer.
+     *	By this way, data communications are data independent.
+     */
+    class CommBuffer {
 
-	friend class ParaTree;
+        friend class ParaTree;
 
-	// =================================================================================== //
-	// MEMBERS																			   //
-	// =================================================================================== //
-	uint32_t m_commBufferSize;
-	char* m_commBuffer;
-	int m_pos;
-	MPI_Comm m_comm;
+        // =================================================================================== //
+        // MEMBERS																			   //
+        // =================================================================================== //
+        uint32_t m_commBufferSize;
+        char* m_commBuffer;
+        int m_pos;
+        MPI_Comm m_comm;
 
-	// =================================================================================== //
-	// CONSTRUCTORS 																	   //
-	// =================================================================================== //
-public:
-	CommBuffer();
-	CommBuffer(MPI_Comm comm_);
-	CommBuffer(uint32_t size, char value, MPI_Comm comm_);
-	CommBuffer(const CommBuffer& other);
-	~CommBuffer();
+        // =================================================================================== //
+        // CONSTRUCTORS 																	   //
+        // =================================================================================== //
+    public:
+        CommBuffer();
+        CommBuffer(MPI_Comm comm_);
+        CommBuffer(uint32_t size, char value, MPI_Comm comm_);
+        CommBuffer(const CommBuffer& other);
+        ~CommBuffer();
 
-	// =================================================================================== //
-	// METHODS                                                                		       //
-	// =================================================================================== //
-	//TODO routines write and read to write and read POD types in buffer
-	CommBuffer& operator=(const CommBuffer& rhs);
+        // =================================================================================== //
+        // METHODS                                                                		       //
+        // =================================================================================== //
+        //TODO routines write and read to write and read POD types in buffer
+        CommBuffer& operator=(const CommBuffer& rhs);
 
-	// =================================================================================== //
-	// TEMPLATE METHODS                                                                    //
-	// =================================================================================== //
+        // =================================================================================== //
+        // TEMPLATE METHODS                                                                    //
+        // =================================================================================== //
 
-	/*! This method writes a MPI-compatible POD datum of type T in commBuffer
-	 * \param[in] val The values that has to be written in the buffer.
-	*/
-	template<class T>
-	void write(T& val) {
-		MPI_Datatype datatype = convert<T>();
-		MPI_Pack(&val,1,datatype,m_commBuffer,m_commBufferSize,&m_pos,m_comm);
-	};
+        /*! This method writes a MPI-compatible POD datum of type T in commBuffer
+         * \param[in] val The values that has to be written in the buffer.
+         */
+        template<class T>
+        void write(T& val) {
+            MPI_Datatype datatype = convert<T>();
+            MPI_Pack(&val,1,datatype,m_commBuffer,m_commBufferSize,&m_pos,m_comm);
+        };
 
-	/*! This method reads from commBuffer the user MPI-compatible POD datum of type T.
-	 * \param[in] val The values that has to be read from the buffer.
-	 */
-	template<class T>
-	void read(T& val) {
-		MPI_Datatype datatype = convert<T>();
-		MPI_Unpack(m_commBuffer,m_commBufferSize,&m_pos,&val,1,datatype,m_comm);
-	};
+        /*! This method reads from commBuffer the user MPI-compatible POD datum of type T.
+         * \param[in] val The values that has to be read from the buffer.
+         */
+        template<class T>
+        void read(T& val) {
+            MPI_Datatype datatype = convert<T>();
+            MPI_Unpack(m_commBuffer,m_commBufferSize,&m_pos,&val,1,datatype,m_comm);
+        };
 
-};
+    };
 
-/* @} */
+    /* @} */
 
 }
 
