9e701f8ca4c4c47fa11e9a091cf6f5fc1bea4fed
Andrea Iob <andrea_iob@hotmail.com>
2017-03-05 10:40:39 +0100

    website: update header and footer

---
+++ documentation/manual/1.2.1/search/functions_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['hasallmetadata',['hasAllMetaData',['../classbitpit_1_1VTKField.html#adb96a40b256c41565604242871e07a29',1,'bitpit::VTKField']]],
+  ['hneighbor',['hNeighbor',['../classbitpit_1_1KdTree.html#a1d32f9409c9d9916c37f6681bcef6487',1,'bitpit::KdTree::hNeighbor(T *, T2, bool, int n=0, int l=0)'],['../classbitpit_1_1KdTree.html#a027370beaa332d65dcc3a9fc66b03474',1,'bitpit::KdTree::hNeighbor(T *, T1 &amp;, T2, int n=0, int l=0)']]],
+  ['hneighbors',['hNeighbors',['../classbitpit_1_1KdTree.html#a06176f3ac6842b701802cdbfddd4fe52',1,'bitpit::KdTree']]]
+];
