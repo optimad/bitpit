a2b385e8cf20ad98259314ab1a4e5ea351cb632c
Andrea Iob <andrea_iob@hotmail.com>
2017-05-30 16:17:44 +0200

    containers/piercedIterator: replace tabs by 4 spaces

--- src/containers/piercedIterator.hpp
+++ src/containers/piercedIterator.hpp
@@ -39,125 +39,125 @@ template<typename value_t, typename id_t>
 class PiercedVector;
 
 /*!
-	\ingroup containers
+    \ingroup containers
 
-	@brief Iterator for the class PiercedVector
+    @brief Iterator for the class PiercedVector
 
-	Usage: use <tt>PiercedVector<value_t, id_t>::iterator</tt>
-	to declare an iterator for a pierced vector, use
-	<tt>PiercedVector<vaue_t, id_t>::const_iterator</tt> to declare
-	a const iterator for a pierced vector.
+    Usage: use <tt>PiercedVector<value_t, id_t>::iterator</tt>
+    to declare an iterator for a pierced vector, use
+    <tt>PiercedVector<vaue_t, id_t>::const_iterator</tt> to declare
+    a const iterator for a pierced vector.
 
-	@tparam value_t The type of the elements stored in the vector
-	@tparam id_t The type of the ids to associate to the elements
+    @tparam value_t The type of the elements stored in the vector
+    @tparam id_t The type of the ids to associate to the elements
 */
 template<typename value_t, typename id_t = long,
          typename value_no_cv_t = typename std::remove_cv<value_t>::type>
 class PiercedIterator
-	: public std::iterator<std::forward_iterator_tag, value_no_cv_t, std::ptrdiff_t, value_t*, value_t&>
+    : public std::iterator<std::forward_iterator_tag, value_no_cv_t, std::ptrdiff_t, value_t*, value_t&>
 {
-	static_assert(std::is_integral<id_t>::value, "Signed integer required for id.");
-	static_assert(std::numeric_limits<id_t>::is_signed, "Signed integer required for id.");
+    static_assert(std::is_integral<id_t>::value, "Signed integer required for id.");
+    static_assert(std::numeric_limits<id_t>::is_signed, "Signed integer required for id.");
 
-	 friend class PiercedIterator<value_no_cv_t, id_t>;
+    friend class PiercedIterator<value_no_cv_t, id_t>;
 
 private:
-	/*!
-		Container.
-	*/
-	template<typename PV_value_t, typename PV_id_t>
-	using Container = PiercedVector<PV_value_t, PV_id_t>;
+    /*!
+        Container.
+    */
+    template<typename PV_value_t, typename PV_id_t>
+    using Container = PiercedVector<PV_value_t, PV_id_t>;
 
-	/*
-		Container type
+    /*
+        Container type
 
-		When building a const_iterator the pointer to the container has to
-		be declared const.
-	*/
-	typedef
-		typename std::conditional<std::is_const<value_t>::value,
-			const Container<value_no_cv_t, id_t>,
-			Container<value_no_cv_t, id_t>
-		>::type
+        When building a const_iterator the pointer to the container has to
+        be declared const.
+    */
+    typedef
+        typename std::conditional<std::is_const<value_t>::value,
+            const Container<value_no_cv_t, id_t>,
+            Container<value_no_cv_t, id_t>
+        >::type
 
-		container_t;
+        container_t;
 
 public:
-	/*!
-		Constant container
-	*/
-	typedef container_t container_type;
+    /*!
+        Constant container
+    */
+    typedef container_t container_type;
 
-	/*!
-		Type of data stored in the container
-	*/
-	typedef value_t value_type;
+    /*!
+        Type of data stored in the container
+    */
+    typedef value_t value_type;
 
-	/*!
-		Type of ids stored in the container
-	*/
-	typedef id_t id_type;
+    /*!
+        Type of ids stored in the container
+    */
+    typedef id_t id_type;
 
-	// Friendships
-	template<typename PV_value_t, typename PV_id_t>
-	friend class PiercedVector;
+    // Friendships
+    template<typename PV_value_t, typename PV_id_t>
+    friend class PiercedVector;
 
-	// Constructors
-	PiercedIterator();
+    // Constructors
+    PiercedIterator();
 
-	// General methods
-	void swap(PiercedIterator& other) noexcept;
+    // General methods
+    void swap(PiercedIterator& other) noexcept;
 
-	container_type & getContainer() const;
+    container_type & getContainer() const;
 
-	// Methos to extract information on the current element
-	id_t getId(const id_t &fallback = -1) const noexcept;
-	std::size_t getRawIndex() const noexcept;
+    // Methos to extract information on the current element
+    id_t getId(const id_t &fallback = -1) const noexcept;
+    std::size_t getRawIndex() const noexcept;
 
-	// Operators
-	PiercedIterator& operator++();
-	PiercedIterator operator++(int);
+    // Operators
+    PiercedIterator& operator++();
+    PiercedIterator operator++(int);
 
-	__PI_REFERENCE__ operator*() const;
-	__PI_POINTER__ operator->() const;
+    __PI_REFERENCE__ operator*() const;
+    __PI_POINTER__ operator->() const;
 
-	template<typename U = value_t, typename U_no_cv = value_no_cv_t,
+    template<typename U = value_t, typename U_no_cv = value_no_cv_t,
              typename std::enable_if<std::is_same<U, U_no_cv>::value, int>::type = 0>
-	operator PiercedIterator<const U_no_cv, id_t>() const;
+    operator PiercedIterator<const U_no_cv, id_t>() const;
 
-	/*!
-		Two-way comparison.
-	*/
-	template<typename other_value_t, typename other_id_t = long,
+    /*!
+        Two-way comparison.
+    */
+    template<typename other_value_t, typename other_id_t = long,
          typename other_value_no_cv_t = typename std::remove_cv<value_t>::type>
-	bool operator==(const PiercedIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
-	{
-		return (m_container == rhs.m_container) && (m_pos == rhs.m_pos);
-	}
+    bool operator==(const PiercedIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
+    {
+        return (m_container == rhs.m_container) && (m_pos == rhs.m_pos);
+    }
 
-	/*!
-		Two-way comparison.
-	*/
-	template<typename other_value_t, typename other_id_t = long,
+    /*!
+        Two-way comparison.
+    */
+    template<typename other_value_t, typename other_id_t = long,
          typename other_value_no_cv_t = typename std::remove_cv<value_t>::type>
-	bool operator!=(const PiercedIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
-	{
-		return (m_container != rhs.m_container) || (m_pos != rhs.m_pos);
-	}
+    bool operator!=(const PiercedIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
+    {
+        return (m_container != rhs.m_container) || (m_pos != rhs.m_pos);
+    }
 
 private:
-	/*!
-		Internal pointer to the container.
-	*/
-	container_t *m_container;
+    /*!
+        Internal pointer to the container.
+    */
+    container_t *m_container;
 
-	/*!
-		Position inside the container.
-	*/
-	size_t m_pos;
+    /*!
+        Position inside the container.
+    */
+    size_t m_pos;
 
-	// Constructors
-	explicit PiercedIterator(container_t *container, const size_t &pos);
+    // Constructors
+    explicit PiercedIterator(container_t *container, const size_t &pos);
 
 };
 
--- src/containers/piercedIterator.tpp
+++ src/containers/piercedIterator.tpp
@@ -28,155 +28,155 @@
 namespace bitpit {
 
 /*!
-	Creates a new uninitialized iterator
+    Creates a new uninitialized iterator
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 PiercedIterator<value_t, id_t, value_no_cv_t>::PiercedIterator()
-	: m_container(nullptr), m_pos(0)
+    : m_container(nullptr), m_pos(0)
 {
 }
 
 /*!
-	Exchanges the values of the current iterator and
-	the iterator recevied as argument.
+    Exchanges the values of the current iterator and
+    the iterator recevied as argument.
 
-	\param other the iterator to exchange values with
+    \param other the iterator to exchange values with
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 void PiercedIterator<value_t, id_t, value_no_cv_t>::swap(PiercedIterator& other) noexcept
 {
-	std::swap(m_container, other.m_container);
-	std::swap(m_pos, other.m_pos);
+    std::swap(m_container, other.m_container);
+    std::swap(m_pos, other.m_pos);
 }
 
 /*!
-	Get a constant reference of the container associated with the iterator.
+    Get a constant reference of the container associated with the iterator.
 
-	\result A constant reference of the container associated with the iterator.
+    \result A constant reference of the container associated with the iterator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 typename PiercedIterator<value_t, id_t, value_no_cv_t>::container_type & PiercedIterator<value_t, id_t, value_no_cv_t>::getContainer() const
 {
-	return *m_container;
+    return *m_container;
 }
 
 /*!
-	Gets the id of the current element.
+    Gets the id of the current element.
 
-	\param fallback is the fallback value to be returned if the iterator
-	points to an invalid position
-	\return The id of the current element or the fallback value if the
-	the iterator points to an invalid position.
+    \param fallback is the fallback value to be returned if the iterator
+    points to an invalid position
+    \return The id of the current element or the fallback value if the
+    the iterator points to an invalid position.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 id_t PiercedIterator<value_t, id_t, value_no_cv_t>::getId(const id_t &fallback) const noexcept
 {
-	id_t id;
-	if (m_pos >= m_container->m_end_pos) {
-		id = fallback;
-		return id;
-	}
+    id_t id;
+    if (m_pos >= m_container->m_end_pos) {
+        id = fallback;
+        return id;
+    }
 
-	id = m_container->m_ids[m_pos];
-	if (id >= 0) {
-		return id;
-	} else {
-		id = fallback;
-		return id;
-	}
+    id = m_container->m_ids[m_pos];
+    if (id >= 0) {
+        return id;
+    } else {
+        id = fallback;
+        return id;
+    }
 }
 
 /*!
-	Gets the position of the current element.
+    Gets the position of the current element.
 
-	\return The position of the current element.
+    \return The position of the current element.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 std::size_t PiercedIterator<value_t, id_t, value_no_cv_t>::getRawIndex() const noexcept
 {
-	return m_pos;
+    return m_pos;
 }
 
 /*!
-	Pre-increment operator.
+    Pre-increment operator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 PiercedIterator<value_t, id_t, value_no_cv_t> & PiercedIterator<value_t, id_t, value_no_cv_t>::operator++()
 {
-	size_t delta = 1;
-	while (true) {
-		m_pos += delta;
-		if (m_pos >= m_container->m_end_pos) {
-			m_pos = m_container->m_end_pos;
-			return *this;
-		}
+    size_t delta = 1;
+    while (true) {
+        m_pos += delta;
+        if (m_pos >= m_container->m_end_pos) {
+            m_pos = m_container->m_end_pos;
+            return *this;
+        }
 
-		id_t id = m_container->m_ids[m_pos];
-		if (id >= 0) {
-			return *this;
-		} else {
-			delta = - id;
-		}
-	}
+        id_t id = m_container->m_ids[m_pos];
+        if (id >= 0) {
+            return *this;
+        } else {
+            delta = - id;
+        }
+    }
 
-	assert(false);
+    assert(false);
 }
 
 /*!
-	Post-increment operator.
+    Post-increment operator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 PiercedIterator<value_t, id_t, value_no_cv_t> PiercedIterator<value_t, id_t, value_no_cv_t>::operator++(int)
 {
-	PiercedIterator tmp(m_container, m_pos);
+    PiercedIterator tmp(m_container, m_pos);
 
-	++(*this);
+    ++(*this);
 
-	return tmp;
+    return tmp;
 }
 
 /*!
-	Deference operator.
+    Deference operator.
 
-	\result A reference to the element currently pointed to by the
-			iterator.
+    \result A reference to the element currently pointed to by the
+            iterator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 __PI_REFERENCE__ PiercedIterator<value_t, id_t, value_no_cv_t>::operator*() const
 {
-	return m_container->m_v[m_pos];
+    return m_container->m_v[m_pos];
 }
 
 /*!
-	Deference operator.
+    Deference operator.
 
-	\result A reference to the element currently pointed to by the
-			iterator.
+    \result A reference to the element currently pointed to by the
+            iterator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 __PI_POINTER__ PiercedIterator<value_t, id_t, value_no_cv_t>::operator->() const
 {
-	return &(m_container->m_v[m_pos]);
+    return &(m_container->m_v[m_pos]);
 }
 
 /*!
-	Converts the iterator to a const_iterator.
+    Converts the iterator to a const_iterator.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 template<typename U, typename U_no_cv,
          typename std::enable_if<std::is_same<U, U_no_cv>::value, int>::type>
 PiercedIterator<value_t, id_t, value_no_cv_t>::operator PiercedIterator<const U_no_cv, id_t>() const
 {
-	return PiercedIterator<const U_no_cv, id_t>(m_container, m_pos);
+    return PiercedIterator<const U_no_cv, id_t>(m_container, m_pos);
 }
 
 /*!
-	Creates a new iterator and initializes it with the position of
-	the const base iterator recevied in input.
+    Creates a new iterator and initializes it with the position of
+    the const base iterator recevied in input.
 */
 template<typename value_t, typename id_t, typename value_no_cv_t>
 PiercedIterator<value_t, id_t, value_no_cv_t>::PiercedIterator(container_t *container, const size_t &pos)
-	: m_container(container), m_pos(pos)
+    : m_container(container), m_pos(pos)
 {
 }
 
