7391a1e3df7a0c0870fcaa512773a8372a9a0d60
Andrea Iob <andrea_iob@hotmail.com>
2017-06-23 10:20:16 +0200

    voloctree: reserve space for interface adaption info

--- src/voloctree/voloctree.cpp
+++ src/voloctree/voloctree.cpp
@@ -998,6 +998,7 @@ const std::vector<adaption::Info> VolOctree::sync(bool updateOctantMaps, bool ge
 			// Adaption info for the deleted interfaces
 			std::size_t adaptionInfoId = adaptionData.create(adaption::TYPE_DELETION, adaption::ENTITY_INTERFACE, currentRank);
 			adaption::Info &adaptionInfo = adaptionData[adaptionInfoId];
+			adaptionInfo.previous.reserve(removedInterfaces.size());
 			for (const long &interfaceId : removedInterfaces) {
 				adaptionInfo.previous.emplace_back();
 				long &deletedInterfaceId = adaptionInfo.previous.back();
@@ -1100,6 +1101,7 @@ const std::vector<adaption::Info> VolOctree::sync(bool updateOctantMaps, bool ge
 			// Adaption info
 			std::size_t infoId = adaptionData.create(adaption::TYPE_CREATION, adaption::ENTITY_INTERFACE, currentRank);
 			adaption::Info &adaptionInfo = adaptionData[infoId];
+			adaptionInfo.current.reserve(createdInterfaces.size());
 			for (const long &interfaceId : createdInterfaces) {
 				adaptionInfo.current.emplace_back();
 				long &createdInterfaceId = adaptionInfo.current.back();
