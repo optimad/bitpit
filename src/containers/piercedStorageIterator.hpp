e8fd306d29985fa9663ccbfebdd2959951cbca98
Andrea Iob <andrea_iob@hotmail.com>
2017-09-04 17:49:28 +0200

    containers/piercedVector: fix template parameters in two-way comparison operators

--- src/containers/piercedStorageIterator.hpp
+++ src/containers/piercedStorageIterator.hpp
@@ -159,7 +159,7 @@ public:
     * Two-way comparison.
     */
     template<typename other_value_t, typename other_id_t = long,
-         typename other_value_no_cv_t = typename std::remove_cv<value_t>::type>
+         typename other_value_no_cv_t = typename std::remove_cv<other_value_t>::type>
     bool operator==(const PiercedStorageIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
     {
         return (PiercedKernelIterator<id_t>::operator==(rhs) && m_storage == rhs.m_storage);
@@ -169,7 +169,7 @@ public:
     * Two-way comparison.
     */
     template<typename other_value_t, typename other_id_t = long,
-         typename other_value_no_cv_t = typename std::remove_cv<value_t>::type>
+         typename other_value_no_cv_t = typename std::remove_cv<other_value_t>::type>
     bool operator!=(const PiercedStorageIterator<other_value_t, other_id_t, other_value_no_cv_t>& rhs) const
     {
         return (PiercedKernelIterator<id_t>::operator!=(rhs) || m_storage != rhs.m_storage);
