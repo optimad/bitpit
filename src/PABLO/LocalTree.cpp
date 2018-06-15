optimad/bitpit-private

PABLO: rename the functions that evalaute the Morton number
Also rename the file that contains those functions.

src/PABLO/LocalTree.cpp
@@ -4064,7 +4064,7 @@ namespace bitpit {
             }
 
             for (auto &node : octnodes){
-                uint64_t morton = keyXYZ(node[0], node[1], node[2], m_global.m_maxLevel);
+                uint64_t morton = PABLO::computeNodeMorton(node[0], node[1], node[2], m_global.m_maxLevel);
                 if (nodeCoords.count(morton) == 0) {
                     mortonList.push_back(morton);
                     nodeCoords.insert({{morton, std::move(node)}});

src/PABLO/ParaTree.cpp
@@ -2782,7 +2782,7 @@ namespace bitpit {
         if (x == m_global.m_maxLength) x = x - 1;
         if (y == m_global.m_maxLength) y = y - 1;
         if (z == m_global.m_maxLength) z = z - 1;
-        morton = mortonEncode_magicbits(x,y,z);
+        morton = PABLO::computeCentroidMorton(x,y,z);
 
 
         powner = 0;
@@ -2861,7 +2861,7 @@ namespace bitpit {
         if (x == m_global.m_maxLength) x = x - 1;
         if (y == m_global.m_maxLength) y = y - 1;
         if (z == m_global.m_maxLength) z = z - 1;
-        morton = mortonEncode_magicbits(x,y,z);
+        morton = PABLO::computeCentroidMorton(x,y,z);
 
 
         powner = 0;

optimad/bitpit

src/PABLO/LocalTree.cpp
@@ -3611,7 +3611,7 @@ namespace bitpit {
             }
 
             for (auto &node : octnodes){
-                uint64_t morton = keyXYZ(node[0], node[1], node[2], m_global.m_maxLevel);
+                uint64_t morton = PABLO::computeXYZKey(node[0], node[1], node[2], m_global.m_maxLevel);
                 if (nodeCoords.count(morton) == 0) {
                     mortonList.push_back(morton);
                     nodeCoords.insert({{morton, std::move(node)}});

src/PABLO/ParaTree.cpp
@@ -2936,7 +2936,7 @@ namespace bitpit {
         if (x == m_global.m_maxLength) x = x - 1;
         if (y == m_global.m_maxLength) y = y - 1;
         if (z == m_global.m_maxLength) z = z - 1;
-        morton = mortonEncode_magicbits(x,y,z);
+        morton = PABLO::computeMorton(x,y,z);
 
 
         powner = 0;
@@ -3015,7 +3015,7 @@ namespace bitpit {
         if (x == m_global.m_maxLength) x = x - 1;
         if (y == m_global.m_maxLength) y = y - 1;
         if (z == m_global.m_maxLength) z = z - 1;
-        morton = mortonEncode_magicbits(x,y,z);
+        morton = PABLO::computeMorton(x,y,z);
 
 
         powner = 0;