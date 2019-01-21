91fe00a81cc24d3f82b2b52e3802416931af0133
Pierre Kestener <pkestene@users.noreply.github.com>
2019-01-21 22:25:53 +0100

    PABLO: ParaTree is a base class derived in PabloUniform; its destructor needs to be virtual

--- src/PABLO/ParaTree.hpp
+++ src/PABLO/ParaTree.hpp
@@ -196,7 +196,7 @@ namespace bitpit {
         ParaTree(uint8_t dim, std::string logfile = DEFAULT_LOG_FILE);
         ParaTree(std::istream &stream, std::string logfile = DEFAULT_LOG_FILE);
 #endif
-        ~ParaTree();
+        virtual ~ParaTree();
 
         ParaTree(const ParaTree & other);
 
