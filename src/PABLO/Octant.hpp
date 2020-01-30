0ed820804515616ef56628fbb513fcab2df5069b
edoardolombardi <edoardo.lombardi@optimad.it>
2020-01-30 19:25:18 +0100

    PABLO: declare get methods of Octant class as public

--- src/PABLO/Octant.hpp
+++ src/PABLO/Octant.hpp
@@ -168,20 +168,19 @@ private:
 public:
 	static unsigned int getBinarySize();
 
+private:
+	// =================================================================================== //
+	// PRIVATE METHODS
+	// =================================================================================== //
+	void initialize();
+	void initialize(uint8_t dim, uint8_t level, bool bound);
+
 public:
 	// =================================================================================== //
 	// PUBLIC METHODS
 	// =================================================================================== //
 	uint64_t		computeFatherMorton() const;
 	u32array3		computeFatherCoordinates() const;
-
-private:
-	// =================================================================================== //
-	// METHODS
-	// =================================================================================== //
-
-	void initialize();
-	void initialize(uint8_t dim, uint8_t level, bool bound);
 
 	// =================================================================================== //
 	// BASIC GET/SET METHODS
@@ -196,7 +195,6 @@ private:
 	int8_t		getMarker() const;
 	bool		getBound(uint8_t face) const;
 	bool		getBound() const;
-	void		setBound(uint8_t face);
 	bool		getPbound(uint8_t face) const;
 	bool		getPbound() const;
 	bool		getIsNewR() const;
@@ -204,6 +202,8 @@ private:
 	bool		getIsGhost() const;
 	int			getGhostLayer() const;
 	bool		getBalance() const;
+protected:
+	void		setBound(uint8_t face);
 	void		setMarker(int8_t marker);
 	void		setBalance(bool balance);
 	void		setLevel(uint8_t level);
@@ -213,6 +213,7 @@ private:
 	// =================================================================================== //
 	// OTHER GET/SET METHODS
 	// =================================================================================== //
+public:
 	uint32_t		getLogicalSize() const;
 	uint64_t		getLogicalArea() const;
 	uint64_t		getLogicalVolume() const;
@@ -234,6 +235,7 @@ private:
 	Octant					buildLastDesc() const;
 	Octant					buildFather() const;
 	std::vector< Octant >	buildChildren() const;
+protected:
 	void computeHalfSizeMortons(uint8_t iface, uint32_t *nMortons, std::vector<uint64_t> *mortons) const;
 	void computeMinSizeMortons(uint8_t iface, uint8_t maxdepth, uint32_t *nMortons, std::vector<uint64_t> *mortons) const;
 	void computeFaceVirtualMortons(uint8_t iface, uint8_t maxdepth, uint32_t *nMortons, std::vector<uint64_t> *mortons) const;
