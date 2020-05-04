1bd4d491e03ff84340cb3d45b22b0dc47d6c3a4d
roccoarpa <rocco.arpa@optimad.it>
2020-05-04 16:54:21 +0200

    IO: reworked copyUntilEOFInString method for Win-MinGW compiling

--- src/IO/GenericIO.cpp
+++ src/IO/GenericIO.cpp
@@ -45,26 +45,55 @@ void flushASCII(std::fstream &str, const uint8_t &data)
 }
 
 /*!
- * Copies entire file into a char array
- * @param[in] str file stream to be copied; file nneds already to be opened
- * @param[out] buffer char array containing entire array
- * @param[out] length number of elements which hvae benn copied
+ *
+ * Copies entire file into a char array.
+ *
+ * NOTE: only valid characters will be copied.
+ *
+ * NOTE: On Windows OS, read-from-file \r\n characters take 2 position in
+ * the stream, but are accounted as a unique character (endline) after C++
+ * translation with read function.
+ *
+ * @param[in] str file stream to be copied; file needs already to be opened
+ * @param[out] buffer char array containing entire array (only valid characters)
+ * @param[out] length number of elements which have been copied
  */
 void copyUntilEOFInString(std::fstream &str, char *&buffer, int &length)
 {
     std::fstream::pos_type position_insert, position_eof;
 
+    // Ask for actual position and end-of-file positions
     position_insert = str.tellg();
-
-    str.seekg(0, str.end);
+    str.seekg(0, std::ios::end);
     position_eof = str.tellg();
 
+    // Evaluate the length on file. This is a prediction of the maximum
+    // number of charactes readable in the current file slot.
     length = position_eof - position_insert;
-    buffer = new char[length];
 
+    // Instantiate a char vector for reading purposes
+    std::vector<char> trybuf(length);
+
+    // Clear and get again the stream to the initial insert position.
+    str.clear();
     str.seekg(position_insert);
-    str.read(buffer, length);
 
+    // Read data
+    str.read(trybuf.data(), length);
+
+    // Resize the trybuf with the effective valid character read.
+    //
+    // This number can be less than length, especially while reading OS-Windows
+    // files.
+    trybuf.resize(str.gcount());
+    length = trybuf.size();
+
+    // Allocate your real buffer and copy effective data in it.
+    buffer = new char[trybuf.size()];
+    std::copy(trybuf.begin(), trybuf.end(), buffer);
+
+    // Clear the stream and get the read stream input to the initial position.
+    str.clear();
     str.seekg(position_insert);
 }
 
