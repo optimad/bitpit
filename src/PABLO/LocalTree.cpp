9b99ea501428a8ef8ed6cc87ca14e744f88dfa9e
Andrea Iob <andrea_iob@hotmail.com>
2020-10-23 15:41:45 +0200

    PABLO: properly check if a node index is valid

--- src/PABLO/LocalTree.cpp
+++ src/PABLO/LocalTree.cpp
@@ -1456,7 +1456,7 @@ namespace bitpit {
         neighbours.clear();
 
         // Default if inode is nnodes<inode<0
-        if (inode > m_treeConstants->nNodes){
+        if (inode >= m_treeConstants->nNodes){
             return;
         }
 
