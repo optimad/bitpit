42de374950aa629eb6431ac310c6a8bb6d94d1b4
Andrea Iob <andrea_iob@hotmail.com>
2021-03-17 17:41:27 +0100

    test/LA: fix usage of DoubleFloatingEqual function

--- test/LA/test_LA_00002.cpp
+++ test/LA/test_LA_00002.cpp
@@ -92,7 +92,7 @@ int subtest_001()
         log::cout() << "  Solution[" << i << "] = " << solution[i] << std::endl;
 
         double expectedSolution = i + 1;
-        if (!utils::DoubleFloatingEqual()(solution[i], expectedSolution, 10)) {
+        if (!utils::DoubleFloatingEqual()(solution[i], expectedSolution, 1e-10)) {
             log::cout() << "  Expected solution[" << i << "] = " << expectedSolution << std::endl;
             log::cout() << "  Error[" << i << "] = " << (expectedSolution - solution[i]) << std::endl;
             throw std::runtime_error("  The solution of the system doesn't match the expected one.");
