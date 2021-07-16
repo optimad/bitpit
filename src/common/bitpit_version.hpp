96f9f5c9947a3a81819a59bd4a72f5a43a9ecb9a
Andrea Iob <andrea_iob@hotmail.com>
2021-07-16 10:56:06 +0200

    common: add a header file with version information
    The header will define both standard and verbose version. Verbose
    version contains information about git and thus it may change every
    time git data changes. If it was included by all the modules, a
    change in git data will force a recompilation of every file that
    includes a bitpit module.

---
+++ src/common/bitpit_version.hpp
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------*\
+ *
+ *  bitpit
+ *
+ *  Copyright (C) 2015-2021 OPTIMAD engineering Srl
+ *
+ *  -------------------------------------------------------------------------
+ *  License
+ *  This file is part of bitpit.
+ *
+ *  bitpit is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License v3 (LGPL)
+ *  as published by the Free Software Foundation.
+ *
+ *  bitpit is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with bitpit. If not, see <http://www.gnu.org/licenses/>.
+ *
+ \*---------------------------------------------------------------------------*/
+
+#ifndef __BITPIT_VERSION_INFO_HPP__
+#define __BITPIT_VERSION_INFO_HPP__
+
+#include "version.hpp"
+#include "version_verbose.hpp"
+
+#endif
