c209c900e7ffe65f1253fda93f33c4f250887a11
Andrea Iob <andrea_iob@hotmail.com>
2021-08-17 20:09:34 +0200

    levelset: implement narrow band cache using levelset storages

--- src/levelset/levelSetCachedObject.cpp
+++ src/levelset/levelSetCachedObject.cpp
@@ -43,6 +43,168 @@
 
 namespace bitpit {
 
+
+/*!
+	@ingroup levelset
+	@interface LevelSetNarrowBandCache
+	@brief Base class for defining narrow band caches.
+*/
+
+/*!
+ * Constructor.
+ */
+LevelSetNarrowBandCache::LevelSetNarrowBandCache() {
+
+    m_values    = addStorage<double>(getStorageCount(), 1, PiercedSyncMaster::SYNC_MODE_JOURNALED);
+    m_gradients = addStorage<std::array<double, 3>>(getStorageCount(), 1, PiercedSyncMaster::SYNC_MODE_JOURNALED);
+
+}
+
+/*!
+ * Get the levelset value of the specified entry.
+ *
+ * \param itr is an iterator pointing to the entry
+ * \result The levelset value of the specified entry.
+ */
+double LevelSetNarrowBandCache::getValue(const KernelIterator &itr) const {
+
+    std::size_t rawId = itr.getRawIndex();
+
+    return m_values->rawAt(rawId);
+
+}
+
+/*!
+ * Get the levelset gradient of the specified entry.
+ *
+ * \param itr is an iterator pointing to the entry
+ * \result The levelset gradient of the specified entry.
+ */
+const std::array<double, 3> & LevelSetNarrowBandCache::getGradient(const KernelIterator &itr) const {
+
+    std::size_t rawId = itr.getRawIndex();
+
+    return m_gradients->rawAt(rawId);
+
+}
+
+/*!
+ * Set the specified cache entry.
+ *
+ * \param itr is an iterator pointing to the cache entry
+ * \param value is the levelset value
+ * \param gradient is the levelset gradient
+ */
+void LevelSetNarrowBandCache::set(const KernelIterator &itr, double value, const std::array<double, 3> &gradient) {
+
+    std::size_t rawId = itr.getRawIndex();
+
+    m_values->rawAt(rawId)    = value;
+    m_gradients->rawAt(rawId) = gradient;
+
+}
+
+/*!
+ * Exchanges the content of the cache with the content the specified other
+ * cache.
+ *
+ * \param other is another cache whose content is swapped with that of this
+ * cache
+ */
+void LevelSetNarrowBandCache::swap(LevelSetNarrowBandCache &other) noexcept
+{
+    LevelSetInternalPiercedStorageManager::swap(other);
+
+    std::swap(other.m_values, m_values);
+    std::swap(other.m_gradients, m_gradients);
+}
+
+/*!
+ * \ingroup levelset
+ * \class LevelSetCachedObjectInterface
+ * \brief The class LevelSetCachedObjectInterface allows to define objects
+ * that cache narrow band information.
+ */
+
+/*!
+ * Initialize the cache.
+ */
+LevelSetNarrowBandCache * LevelSetCachedObjectInterface::initializeNarrowBandCache()
+{
+    m_narrowBandCache = createNarrowBandCache();
+
+    return getNarrowBandCache();
+}
+
+/*!
+ * Get a pointer to the cache.
+ *
+ * \result A pointer to the cache.
+ */
+LevelSetNarrowBandCache * LevelSetCachedObjectInterface::getNarrowBandCache()
+{
+    return m_narrowBandCache.get();
+}
+
+/*!
+ * Get a constant pointer to the cache.
+ *
+ * \result A constant pointer to the cache.
+ */
+const LevelSetNarrowBandCache * LevelSetCachedObjectInterface::getNarrowBandCache() const
+{
+    return m_narrowBandCache.get();
+}
+
+/*!
+ * Clear the storage.
+ */
+void LevelSetCachedObjectInterface::clearNarrowBandCache()
+{
+    // Narrowband cache
+    if (m_narrowBandCache) {
+        m_narrowBandCache->clear();
+    }
+}
+
+/*!
+ * Dump the storage.
+ *
+ * \param stream is the output stream
+ */
+void LevelSetCachedObjectInterface::dumpNarrowBandCache(std::ostream &stream)
+{
+    // Narrowband cache
+    if (m_narrowBandCache) {
+        m_narrowBandCache->dump( stream );
+    }
+}
+
+/*!
+ * Restore the storage.
+ *
+ * \param stream is the input stream
+ */
+void LevelSetCachedObjectInterface::restoreNarrowBandCache(std::istream &stream)
+{
+    // Narrowband cache
+    if (m_narrowBandCache) {
+        m_narrowBandCache->restore( stream );
+    }
+}
+
+/*!
+ * Exchanges the content of the object with the content the specified other
+ * object.
+ *
+ * \param other is another object whose content is swapped with that of this
+ * object
+ */
+void LevelSetCachedObjectInterface::swap(LevelSetCachedObjectInterface &other) noexcept
+{
+    m_narrowBandCache.swap(other.m_narrowBandCache);
+}
+
 /*!
 	@ingroup levelset
 	@interface LevelSetCachedObject
@@ -54,10 +216,29 @@ namespace bitpit {
  * @param[in] id id assigned to object
  */
 LevelSetCachedObject::LevelSetCachedObject(int id)
-    : LevelSetObject(id),
-      m_narrowBandValues(1, &m_narrowBandKernel, PiercedSyncMaster::SYNC_MODE_JOURNALED),
-      m_narrowBandGradients(1, &m_narrowBandKernel, PiercedSyncMaster::SYNC_MODE_JOURNALED)
+    : LevelSetObject(id)
 {
+}
+
+/*!
+ * Sets the kernel for the object
+ * @param[in] kernel is the LevelSetKernel
+ */
+void LevelSetCachedObject::setKernel(LevelSetKernel *kernel) {
+
+    LevelSetObject::setKernel(kernel);
+
+    initializeNarrowBandCache();
+
+}
+
+/*!
+ * Create the storage for the narrow band data.
+ */
+std::shared_ptr<LevelSetNarrowBandCache> LevelSetCachedObject::createNarrowBandCache() {
+
+    return std::shared_ptr<LevelSetNarrowBandCache>(new LevelSetNarrowBandCache());
+
 }
 
 /*!
@@ -79,10 +260,11 @@ std::shared_ptr<LevelSetSignStorage> LevelSetCachedObject::createSignStorage() {
 */
 LevelSetInfo LevelSetCachedObject::getLevelSetInfo( long id)const{
 
-    auto narrowBandItr = m_narrowBandKernel.find(id) ;
-    if( narrowBandItr != m_narrowBandKernel.end() ){
-        double value = m_narrowBandValues.rawAt(narrowBandItr.getRawIndex());
-        const std::array<double, 3> &gradient = m_narrowBandGradients.rawAt(narrowBandItr.getRawIndex());
+    const LevelSetNarrowBandCache *narrowBandCache = getNarrowBandCache();
+    LevelSetNarrowBandCache::KernelIterator narrowBandCacheItr = narrowBandCache->find(id) ;
+    if( narrowBandCacheItr != narrowBandCache->end() ){
+        double value = narrowBandCache->getValue(narrowBandCacheItr);
+        const std::array<double, 3> &gradient = narrowBandCache->getGradient(narrowBandCacheItr);
 
         return LevelSetInfo(value, gradient);
     }
@@ -109,9 +291,10 @@ double LevelSetCachedObject::getLS( long id)const {
  */
 double LevelSetCachedObject::getValue( long id)const {
 
-    auto narrowBandItr = m_narrowBandKernel.find(id) ;
-    if( narrowBandItr != m_narrowBandKernel.end() ){
-        return m_narrowBandValues.rawAt(narrowBandItr.getRawIndex());
+    const LevelSetNarrowBandCache *narrowBandCache = getNarrowBandCache();
+    LevelSetNarrowBandCache::KernelIterator narrowBandCacheItr = narrowBandCache->find(id) ;
+    if( narrowBandCacheItr != narrowBandCache->end() ){
+        return narrowBandCache->getValue(narrowBandCacheItr);
     }
 
     return getSign(id) * levelSetDefaults::VALUE;
@@ -126,17 +309,19 @@ double LevelSetCachedObject::getValue( long id)const {
 short LevelSetCachedObject::getSign( long id ) const {
 
     // Check if the sign can be evaluated from narrowband value
-    auto narrowBandItr = m_narrowBandKernel.find(id) ;
-    if( narrowBandItr != m_narrowBandKernel.end() ){
-        double value = m_narrowBandValues.rawAt(narrowBandItr.getRawIndex());
+    const LevelSetNarrowBandCache *narrowBandCache = getNarrowBandCache();
+    LevelSetNarrowBandCache::KernelIterator narrowBandCacheItr = narrowBandCache->find(id) ;
+    if( narrowBandCacheItr != narrowBandCache->end() ){
+        double value = narrowBandCache->getValue(narrowBandCacheItr);
 
         return evalValueSign(value);
     }
 
     // Check if the sign can be evaluated from the propagation
     if (!isSignStorageDirty()) {
-        LevelSetSignStorage::KernelIterator signStorageItr = getSignStorage()->find(id);
-        LevelSetSignStorage::Sign propagatedSign = getSignStorage()->at(signStorageItr);
+        const LevelSetSignStorage *propagatedSignStorage = getSignStorage();
+        LevelSetSignStorage::KernelIterator propagatedSignStorageItr = propagatedSignStorage->find(id);
+        LevelSetSignStorage::Sign propagatedSign = propagatedSignStorage->at(propagatedSignStorageItr);
         if (propagatedSign != LevelSetSignStorage::SIGN_UNDEFINED) {
             return static_cast<short>(propagatedSign);
         }
@@ -156,9 +341,10 @@ short LevelSetCachedObject::getSign( long id ) const {
  */
 std::array<double,3> LevelSetCachedObject::getGradient(long id) const {
 
-    auto narrowBandItr = m_narrowBandKernel.find(id) ;
-    if( narrowBandItr != m_narrowBandKernel.end() ){
-        return m_narrowBandGradients.rawAt(narrowBandItr.getRawIndex());
+    const LevelSetNarrowBandCache *narrowBandCache = getNarrowBandCache();
+    LevelSetNarrowBandCache::KernelIterator narrowBandCacheItr = narrowBandCache->find(id) ;
+    if( narrowBandCacheItr != narrowBandCache->end() ){
+        return narrowBandCache->getGradient(narrowBandCacheItr);
     }
 
     return levelSetDefaults::GRADIENT;
@@ -172,20 +358,20 @@ std::array<double,3> LevelSetCachedObject::getGradient(long id) const {
 void LevelSetCachedObject::_clearAfterMeshAdaption( const std::vector<adaption::Info> &mapper ){
 
     // Clear stale narrow band entries
+    LevelSetNarrowBandCache *narrowBandCache = getNarrowBandCache();
     for (const adaption::Info &adaptionInfo : mapper) {
         if (adaptionInfo.entity != adaption::Entity::ENTITY_CELL) {
             continue;
         }
 
         for (long cellId : adaptionInfo.previous) {
-            if (containsNarrowBandEntry(cellId)) {
-                eraseNarrowBandEntry(cellId, false);
+            if (narrowBandCache->contains(cellId)) {
+                narrowBandCache->erase(cellId, false);
             }
         }
     }
 
-    syncNarrowBandStorages();
-
+    narrowBandCache->syncStorages();
 
 }
 
@@ -195,8 +381,7 @@ void LevelSetCachedObject::_clearAfterMeshAdaption( const std::vector<adaption::
 void LevelSetCachedObject::_clear( ){
 
     // Clear narrow band entries
-    m_narrowBandKernel.clear() ;
-    syncNarrowBandStorages() ;
+    clearNarrowBandCache();
 
     // Clear sign propgation storage
     clearSignStorage();
@@ -209,7 +394,7 @@ void LevelSetCachedObject::_clear( ){
  * @return true/false if the centroid is in narrow band
  */
 bool LevelSetCachedObject::isInNarrowBand(long id)const{
-    return containsNarrowBandEntry(id);
+    return getNarrowBandCache()->contains(id);
 }
 
 /*!
@@ -218,12 +403,8 @@ bool LevelSetCachedObject::isInNarrowBand(long id)const{
  */
 void LevelSetCachedObject::_dump( std::ostream &stream ){
 
-    // Narrow band kernel
-    m_narrowBandKernel.dump( stream );
-
-    // Narrow band storages
-    m_narrowBandValues.dump( stream );
-    m_narrowBandGradients.dump( stream );
+    // Narrow band storage
+    dumpNarrowBandCache( stream );
 
     // Stored sign
     dumpSignStorage( stream );
@@ -235,12 +416,8 @@ void LevelSetCachedObject::_dump( std::ostream &stream ){
  */
 void LevelSetCachedObject::_restore( std::istream &stream ){
 
-    // Narrow band kernel
-    m_narrowBandKernel.restore( stream );
-
-    // Narrow band storages
-    m_narrowBandValues.restore( stream );
-    m_narrowBandGradients.restore( stream );
+    // Narrow band storage
+    restoreNarrowBandCache( stream );
 
     // Stored sign
     restoreSignStorage( stream );
@@ -258,32 +435,7 @@ void LevelSetCachedObject::_restore( std::istream &stream ){
  */
 void LevelSetCachedObject::_writeCommunicationBuffer( const std::vector<long> &sendList, SendBuffer &dataBuffer ){
 
-    // Evaluate the size of the buffer
-    std::size_t nInfoItems = 0;
-    for( long id : sendList){
-        if( containsNarrowBandEntry(id)){
-            ++nInfoItems ;
-        }
-    }
-
-    dataBuffer.setSize(dataBuffer.getSize() + sizeof(std::size_t) + nInfoItems* getNarrowBandEntryBinarySize() ) ;
-
-    // Fill the buffer
-    dataBuffer << nInfoItems ;
-
-    for( std::size_t k = 0; k < sendList.size(); ++k){
-        long id = sendList[k];
-        NarrowBandIterator narrowBandItr = getNarrowBandEntryIterator(id) ;
-        if( narrowBandItr == m_narrowBandKernel.end() ){
-            continue;
-        }
-
-        // Write the index of the cell
-        dataBuffer << k ;
-
-        // Write the narrowband entry
-        writeNarrowBandEntryCommunicationBuffer(narrowBandItr, dataBuffer);
-    }
+    getNarrowBandCache()->write(sendList, dataBuffer);
 }
 
 /*!
@@ -296,157 +448,9 @@ void LevelSetCachedObject::_writeCommunicationBuffer( const std::vector<long> &s
  */
 void LevelSetCachedObject::_readCommunicationBuffer( const std::vector<long> &recvList, RecvBuffer &dataBuffer ){
 
-    std::size_t nInfoItems ;
-    dataBuffer >> nInfoItems ;
-
-    for( std::size_t i=0; i<nInfoItems; ++i){
-        // Read the id of the cell
-        std::size_t k ;
-        dataBuffer >> k ;
-        long id = recvList[k] ;
-
-        // Create the narrowband entry
-        NarrowBandIterator narrowBandItr = getNarrowBandEntryIterator(id) ;
-        if( narrowBandItr == m_narrowBandKernel.end() ){
-            narrowBandItr = createNarrowBandEntry(id);
-        }
-
-        // Fill the information associated to the narrowband entry
-        readNarrowBandEntryCommunicationBuffer(narrowBandItr, dataBuffer);
-    }
+    getNarrowBandCache()->read(recvList, dataBuffer);
 }
 
-#endif
-
-/*!
- * Create a new narrow band entry.
- *
- * \param id is id of the cell
- * \param sync controls if the kernel will be synched
- * \result An iterator pointing to the newly created narrowband kernel entry.
- */
-LevelSetCachedObject::NarrowBandIterator LevelSetCachedObject::createNarrowBandEntry(long id, bool sync) {
-
-    NarrowBandKernel::FillAction action = m_narrowBandKernel.fillHead(id) ;
-    if (sync) {
-        syncNarrowBandStorages() ;
-    }
-
-    return m_narrowBandKernel.rawFind(action.info[PiercedSyncAction::INFO_POS]);
-
-}
-
-/*!
- * Delete a narrow band entry.
- *
- * \param id is id of the cell
- * \param sync controls if the kernel will be synched
- */
-void LevelSetCachedObject::eraseNarrowBandEntry(long id, bool sync) {
-
-    m_narrowBandKernel.erase(id, sync) ;
-    if (sync) {
-        syncNarrowBandStorages() ;
-    }
-
-}
-
-/*!
- * Checks if the object contains a narrow band entry for the specified cell.
- *
- * \param id is id of the cell
- * \result Return true is the object contains a narrow band entry for the
- * specified cell, false otherwise.
- */
-bool LevelSetCachedObject::containsNarrowBandEntry(long id) const {
-
-    return m_narrowBandKernel.contains(id) ;
-
-}
-
-/*!
- * Get an iterator pointing to the narrowband entry associated with the
- * specified cell.
- *
- * \param id is id of the cell
- * \result An iterator pointing to the narrowband entry associated with the
- * specified cell.
- */
-LevelSetCachedObject::NarrowBandIterator LevelSetCachedObject::getNarrowBandEntryIterator(long id) const {
-
-    return m_narrowBandKernel.find(id);
-
-}
-
-/*!
- * Set the information associated with the specified narrow band entry.
- *
- * \param itr is an iterator pointing to the narrowband entry
- * \param value is the levelset value
- * \param gradient is the levelset gradient
- */
-void LevelSetCachedObject::setNarrowBandEntry(NarrowBandIterator itr, double value, const std::array<double, 3> &gradient) {
-
-    std::size_t rawId = itr.getRawIndex();
-
-    m_narrowBandValues.rawAt(rawId)    = value;
-    m_narrowBandGradients.rawAt(rawId) = gradient;
-
-}
-
-/*!
- * Synchronize narrow band storages with the kernel.
- */
-void LevelSetCachedObject::syncNarrowBandStorages() {
-
-    m_narrowBandKernel.flush() ;
-    m_narrowBandKernel.sync() ;
-
-}
-
-#if BITPIT_ENABLE_MPI
-/*!
- * Get the size, expressed in bytes, of a narrowband entry.
- *
- * \result The size, expressed in bytes, of a narrowband entry.
- */
-std::size_t LevelSetCachedObject::getNarrowBandEntryBinarySize() const {
-
-    std::size_t entrySize = sizeof(double) + sizeof(std::array<double, 3>) ;
-
-    return entrySize;
-
-}
-
-/*!
- * Write narrow band entry into the communication buffer.
- *
- * \param narrowBandItr is an iterator pointing to the narrowband entry
- * \param[in,out] dataBuffer is the communication buffer
- */
-void LevelSetCachedObject::writeNarrowBandEntryCommunicationBuffer( NarrowBandIterator narrowBandItr, SendBuffer &dataBuffer ){
-
-    std::size_t narrowBandRawId = narrowBandItr.getRawIndex();
-
-    dataBuffer << m_narrowBandValues.rawAt(narrowBandRawId);
-    dataBuffer << m_narrowBandGradients.rawAt(narrowBandRawId);
-
-}
-
-/*!
- * Read narrow band entry from the communication buffer.
- *
- * \param narrowBandItr is an iterator pointing to the narrowband entry
- * \param[in,out] dataBuffer is the communication buffer
- */
-void LevelSetCachedObject::readNarrowBandEntryCommunicationBuffer( NarrowBandIterator narrowBandItr, RecvBuffer &dataBuffer ){
-
-    std::size_t narrowBandRawId = narrowBandItr.getRawIndex();
-
-    dataBuffer >> m_narrowBandValues.rawAt(narrowBandRawId);
-    dataBuffer >> m_narrowBandGradients.rawAt(narrowBandRawId);
-
-}
 #endif
 
 }
