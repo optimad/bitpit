8bd6b8c0cf6d07fdf96107ee220d53889775fc81
Andrea Iob <andrea_iob@hotmail.com>
2022-06-26 14:40:49 +0200

    manual: update script for generating the manual

--- _manual/generate.sh
+++ _manual/generate.sh
@@ -11,7 +11,7 @@ MANUAL_BUILD_DIR="${MANUAL_ROOT_DIR}/build"
 # Generate build directory
 rm -rf "${MANUAL_BUILD_DIR}"
 mkdir "${MANUAL_BUILD_DIR}"
-mkdir "${MANUAL_BUILD_DIR}/templates"
+mkdir "${MANUAL_BUILD_DIR}/_doxygen_templates"
 cd "${MANUAL_BUILD_DIR}"
 
 # Link files needed by jekyll
@@ -20,8 +20,8 @@ cp -r "${MANUAL_ROOT_DIR}/_layouts" "${MANUAL_BUILD_DIR}"
 cp -r "${MANUAL_ROOT_DIR}/stylesheets" "${MANUAL_BUILD_DIR}"
 cp "${JEKYLL_ROOT_DIR}/_includes/"* "${MANUAL_BUILD_DIR}/_includes/"
 cp "${JEKYLL_ROOT_DIR}/_layouts/"* "${MANUAL_BUILD_DIR}/_layouts/"
-cp "${MANUAL_ROOT_DIR}/doxygen_footer.html" "${MANUAL_BUILD_DIR}/templates"
-cp "${MANUAL_ROOT_DIR}/doxygen_header.html" "${MANUAL_BUILD_DIR}/templates"
+cp "${MANUAL_ROOT_DIR}/doxygen_footer.html" "${MANUAL_BUILD_DIR}"
+cp "${MANUAL_ROOT_DIR}/doxygen_header.html" "${MANUAL_BUILD_DIR}"
 
 # Configration files needed by jekyll
 JEKYLL_CONFIG_GLOBAL="${JEKYLL_ROOT_DIR}/_config.yml"
@@ -48,33 +48,31 @@ cd ${JEKYLL_ROOT_DIR}
 bundle exec jekyll build \
     --config "${JEKYLL_CONFIG_LIST}" \
     --source ${MANUAL_BUILD_DIR} \
-    --destination ${MANUAL_BUILD_DIR}/templates
+    --destination ${MANUAL_BUILD_DIR}/doxygen_templates
 
 # Generate doxygen files
-export LD_LIBRARY_PATH=/usr/lib/hpc/gnu10/openmpi3/hdf5/1.10.7/lib64/:/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/lib64/:/usr/lib64/mpi/gcc/openmpi2/lib64
-
 cd ${MANUAL_BUILD_DIR}
 mkdir bitpit
 cd bitpit
 
 cmake ${BITPIT_SOURCE_DIR} \
     -DCMAKE_INSTALL_PREFIX:PATH="" \
-    -DBUILD_DOCUMENTATION=ON \
+    -DBITPIT_ENABLE_DOC="ON" \
+    -DBITPIT_ENABLE_MPI="ON" \
     -DBLAS_VENDOR="Generic" \
     -DLAPACKE_STATIC="ON" \
-    -DENABLE_MPI="ON" \
     -DPETSC_ARCH="" \
-    -DPETSC_DIR="/usr/lib/hpc/gnu10/openmpi3/petsc/3.14.4" \
-    -DMPI_INCLUDE_PATH="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/include/" \
-    -DMPIEXEC_EXECUTABLE="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/bin/mpiexec" \
-    -DMPI_CXX_COMPILER="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/bin/mpicxx" \
-    -DMPI_CXX_HEADER_DIR="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/include" \
+    -DPETSC_DIR="/usr/lib64/mpi/gcc/openmpi4/lib64/petsc/3.14.5/linux-gnu-c-opt" \
+    -DMPI_INCLUDE_PATH="/usr/lib64/mpi/gcc/openmpi4/include/" \
+    -DMPIEXEC_EXECUTABLE="/usr/lib64/mpi/gcc/openmpi4/bin/mpiexec" \
+    -DMPI_CXX_COMPILER="/usr/lib64/mpi/gcc/openmpi4/bin/mpicxx" \
+    -DMPI_CXX_HEADER_DIR="/usr/lib64/mpi/gcc/openmpi4/include" \
     -DMPI_CXX_LIB_NAMES="mpi_mpifh;mpi" \
     -DMPI_CXX_LINK_FLAGS="-pthread" \
-    -DMPI_mpi_LIBRARY="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/lib64/libmpi_mpifh.so;/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/lib64/libmpi.so" \
-    -DMPI_mpi_mpifh_LIBRARY="/usr/lib/hpc/gnu10/mpi/openmpi/3.1.6/lib64/libmpi_mpifh.so" \
-    -DDOXY_HTML_HEADER="${MANUAL_BUILD_DIR}/templates/doxygen_header.html" \
-    -DDOXY_HTML_FOOTER="${MANUAL_BUILD_DIR}/templates/doxygen_footer.html" \
+    -DMPI_mpi_LIBRARY="/usr/lib64/mpi/gcc/openmpi4/lib64/libmpi_mpifh.so;/usr/lib64/mpi/gcc/openmpi4/lib64/libmpi.so" \
+    -DMPI_mpi_mpifh_LIBRARY="/usr/lib64/mpi/gcc/openmpi4/lib64/libmpi_mpifh.so" \
+    -DDOXY_HTML_HEADER="${MANUAL_BUILD_DIR}/doxygen_templates/doxygen_header.html" \
+    -DDOXY_HTML_FOOTER="${MANUAL_BUILD_DIR}/doxygen_templates/doxygen_footer.html" \
     -DDOXY_HTML_EXTRA_STYLESHEET="${MANUAL_BUILD_DIR}/stylesheets/manual.css" \
     -DDOXY_DISABLE_INDEX="NO" \
     -DDOXY_GENERATE_TREEVIEW="NO"
