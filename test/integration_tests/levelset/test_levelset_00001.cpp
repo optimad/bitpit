024cc91acb8eaa582e9e0129e2471941ffd8f317
Andrea Iob <andrea_iob@hotmail.com>
2023-06-16 10:39:55 +0200

    test: test basic features of levelset also on octree meshes

--- test/integration_tests/levelset/test_levelset_00001.cpp
+++ test/integration_tests/levelset/test_levelset_00001.cpp
@@ -40,82 +40,85 @@
 # include "bitpit_levelset.hpp"
 # include "bitpit_surfunstructured.hpp"
 # include "bitpit_volcartesian.hpp"
+# include "bitpit_voloctree.hpp"
 
 /*!
 * Generate a 2D surface mesh.
 *
-* \param[in,out] mesh on output it will contain the generated surface mesh
+* \param dimension is the dimension of the patch
 */
-void Generate2DSurfMesh( bitpit::SurfUnstructured &mesh )
+std::unique_ptr<bitpit::SurfUnstructured> generateSegmentation(int dimension)
 {
-
+    // Surface parameters
     const double R = 1.0;
-    const long N = 32;
-    double theta;
-    double dtheta = 2. * BITPIT_PI/((double) N);
-    
-    std::array<double,3> point;
-    std::vector<long> connect(2, bitpit::Element::NULL_ID);
+    const long   N = 32;
+
+    // Creat an empty patch
+#if BITPIT_ENABLE_MPI
+    auto segmentation = std::unique_ptr<bitpit::SurfUnstructured>(new bitpit::SurfUnstructured(dimension, MPI_COMM_NULL));
+#else
+    auto segmentation = std::unique_ptr<bitpit::SurfUnstructured>(new bitpit::SurfUnstructured(dimension));
+#endif
 
     // Create vertex list
     // Use non-consecutive vertex ids to test if the levelset can handle them.
     const long vertexIdOffset = 101;
     const long vertexIdStride = 2;
 
+    double dtheta = 2. * BITPIT_PI/((double) N);
+
+    std::array<double,3> point;
     point[2] = 0.0;
     for (long i = 0; i < N; ++i) {
-        theta = ((double) i) * dtheta;
+        double theta = ((double) i) * dtheta;
         point[0] = R * cos( theta );
         point[1] = R * sin( theta );
-        mesh.addVertex(point, vertexIdOffset + vertexIdStride * i);
+        segmentation->addVertex(point, vertexIdOffset + vertexIdStride * i);
     }
 
     // Create simplex list
+    //
     // Use non-consecutive cell ids to test if the levelset can handle them.
+    std::vector<long> connect(2, bitpit::Element::NULL_ID);
+
     const long cellIdOffset = 202;
     const long cellIdStride = 3;
 
     for (long i = 0; i < N; ++i) {
         connect[0] = vertexIdOffset + vertexIdStride * i;
         connect[1] = vertexIdOffset + vertexIdStride * ((i + 1) % N);
-        mesh.addCell(bitpit::ElementType::LINE, connect, cellIdOffset + cellIdStride * i);
+        segmentation->addCell(bitpit::ElementType::LINE, connect, cellIdOffset + cellIdStride * i);
     }
 
+    return segmentation;
 }
 
 /*!
 * Subtest 001
 *
-* Testing basic features of a 2D levelset.
+* Testing basic features of a 2D levelset on a Certesian mesh.
 */
 int subtest_001()
 {
     int dimensions(2) ;
 
-    // Input geometry
-#if BITPIT_ENABLE_MPI
-    std::unique_ptr<bitpit::SurfUnstructured> STL( new bitpit::SurfUnstructured(dimensions - 1,MPI_COMM_NULL) );
-#else
-    std::unique_ptr<bitpit::SurfUnstructured> STL( new bitpit::SurfUnstructured(dimensions - 1) );
-#endif
-    STL->initializeAdjacencies();
+    // Initialize segmentation
+    bitpit::log::cout() << " - Generating segmentation" << std::endl;
 
-    bitpit::log::cout() << " - Loading dgf geometry" << std::endl;
+    std::unique_ptr<bitpit::SurfUnstructured> segmentation = generateSegmentation(dimensions - 1);
+    segmentation->initializeAdjacencies();
+    segmentation->getVTK().setName("geometry_cartesian_001") ;
+    segmentation->write() ;
 
-    Generate2DSurfMesh( *(STL.get()) ) ;
+    bitpit::log::cout() << "n. vertex: " << segmentation->getVertexCount() << std::endl;
+    bitpit::log::cout() << "n. simplex: " << segmentation->getCellCount() << std::endl;
 
-    STL->getVTK().setName("geometry_001") ;
-    STL->write() ;
-
-    bitpit::log::cout() << "n. vertex: " << STL->getVertexCount() << std::endl;
-    bitpit::log::cout() << "n. simplex: " << STL->getCellCount() << std::endl;
-
-    // create cartesian mesh around geometry 
-    bitpit::log::cout() << " - Setting mesh" << std::endl;
+    // Initialize mesh
+    bitpit::log::cout() << " - Generating mesh" << std::endl;
     std::array<double,3> meshMin, meshMax, delta ;
     std::array<int,3> nc = {{64, 64, 0}} ;
 
-    STL->getBoundingBox( meshMin, meshMax ) ;
+    segmentation->getBoundingBox( meshMin, meshMax ) ;
 
     delta = meshMax -meshMin ;
     meshMin -=  0.1*delta ;
@@ -147,7 +150,7 @@ int subtest_001()
 
     levelset.setMesh(&mesh) ;
 
-    int id0 = levelset.addObject(std::move(STL),BITPIT_PI) ;
+    int id0 = levelset.addObject(std::move(segmentation),BITPIT_PI) ;
     int id1 = levelset.addObject(mask) ;
     std::vector<int> ids;
     ids.push_back(id0);
@@ -164,7 +167,102 @@ int subtest_001()
 
     levelset.getObject(id0).enableVTKOutput(bitpit::LevelSetWriteField::VALUE);
     levelset.getObject(id1).enableVTKOutput(bitpit::LevelSetWriteField::VALUE);
-    mesh.getVTK().setName("levelset_001") ;
+    mesh.getVTK().setName("levelset_cartesian_001") ;
+    mesh.write() ;
+
+    bitpit::log::cout() << " - Exported data" << std::endl;
+
+    return 0;
+}
+
+/*!
+* Subtest 002
+*
+* Testing basic features of a 2D levelset on an Octree mesh.
+*/
+int subtest_002()
+{
+    int dimensions(2) ;
+
+    // Initialize segmentation
+    bitpit::log::cout() << " - Generating segmentation" << std::endl;
+
+    std::unique_ptr<bitpit::SurfUnstructured> segmentation = generateSegmentation(dimensions - 1);
+    segmentation->initializeAdjacencies();
+    segmentation->getVTK().setName("geometry_cartesian_001") ;
+    segmentation->write() ;
+
+    bitpit::log::cout() << "n. vertex: " << segmentation->getVertexCount() << std::endl;
+    bitpit::log::cout() << "n. simplex: " << segmentation->getCellCount() << std::endl;
+
+    // Initialize mesh
+    bitpit::log::cout() << " - Generating mesh" << std::endl;
+
+    std::array<double, 3> segmentationMin;
+    std::array<double, 3> segmentationMax;
+    segmentation->getBoundingBox(segmentationMin, segmentationMax);
+
+    std::array<double, 3> delta = segmentationMax - segmentationMin;
+    segmentationMin -= 0.1 * delta;
+    segmentationMax += 0.1 * delta;
+    delta = segmentationMax - segmentationMin;
+
+    std::array<double, 3> origin = segmentationMin;
+
+    double length = 0.;
+    for (int i = 0; i < 3; ++i) {
+        length = std::max(length, segmentationMax[i] - segmentationMin[i]);
+    };
+
+    double dh = length / 64;
+
+#if BITPIT_ENABLE_MPI
+    bitpit::VolOctree mesh(dimensions, origin, length, dh, MPI_COMM_NULL);
+#else
+    bitpit::VolOctree mesh(dimensions, origin, length, dh);
+#endif
+
+    mesh.update() ;
+    mesh.initializeAdjacencies() ;
+    mesh.initializeInterfaces() ;
+
+    // mark cells within R=0.5
+    std::unordered_set<long> mask;
+    for( auto & cell : mesh.getCells() ){
+        long id = cell.getId() ;
+        std::array<double,3> center = mesh.evalCellCentroid(id);
+        double r = norm2(center);
+        if(r<=0.5){
+            mask.insert(id);
+        }
+    }
+
+    // Compute level set in narrow band
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    int elapsed_seconds;
+
+    bitpit::LevelSet levelset ;
+
+    levelset.setMesh(&mesh) ;
+
+    int id0 = levelset.addObject(std::move(segmentation),BITPIT_PI) ;
+    int id1 = levelset.addObject(mask) ;
+    std::vector<int> ids;
+    ids.push_back(id0);
+    ids.push_back(id1);
+
+    start = std::chrono::system_clock::now();
+    levelset.compute( ) ;
+    end = std::chrono::system_clock::now();
+
+    elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    bitpit::log::cout() << "elapsed time: " << elapsed_seconds << " ms" << std::endl;
+
+    bitpit::log::cout() << " - Exporting data" << std::endl;
+
+    levelset.getObject(id0).enableVTKOutput(bitpit::LevelSetWriteField::VALUE);
+    levelset.getObject(id1).enableVTKOutput(bitpit::LevelSetWriteField::VALUE);
+    mesh.getVTK().setName("levelset_octree_001") ;
     mesh.write() ;
 
     bitpit::log::cout() << " - Exported data" << std::endl;
@@ -193,6 +291,11 @@ int main(int argc, char *argv[])
 	int status;
 	try {
 		status = subtest_001();
+		if (status != 0) {
+			return status;
+		}
+
+		status = subtest_002();
 		if (status != 0) {
 			return status;
 		}
