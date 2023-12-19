89308cfca503dd10b5c629815c61c943c9ca288d
roccoarpa <rocco.arpa@optimad.it>
2023-12-19 12:58:39 +0100

    bitpit: add support for PETSc when compiling windows with MSVC

--- external/PETSc/cmake/CorrectWindowsPaths.cmake
+++ external/PETSc/cmake/CorrectWindowsPaths.cmake
@@ -3,10 +3,18 @@
 # CONVERT_CYGWIN_PATH( PATH )
 #  This uses the command cygpath (provided by cygwin) to convert
 #  unix-style paths into paths useable by cmake on windows
+#
+# The variable PETSC_CYGPATH_EXECUTABLE can be used to specify the path
+# of cygpath.
 
 macro (CONVERT_CYGWIN_PATH _path)
   if (WIN32)
-    EXECUTE_PROCESS(COMMAND cygpath.exe -m ${${_path}}
+    set(CYGPATH_EXECUTABLE "cygpath.exe")
+    if (PETSC_CYGPATH_DIR)
+      set(CYGPATH_EXECUTABLE "${PETSC_CYGPATH_DIR}/${CYGPATH_EXECUTABLE}")
+    endif (PETSC_CYGPATH_DIR)
+
+    EXECUTE_PROCESS(COMMAND ${CYGPATH_EXECUTABLE} -m ${${_path}}
       OUTPUT_VARIABLE ${_path})
     string (STRIP ${${_path}} ${_path})
   endif (WIN32)
