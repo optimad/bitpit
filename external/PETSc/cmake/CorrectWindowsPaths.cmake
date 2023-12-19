9d66fdecc1a6f2a6bb057f95e741d97152d2c48e
roccoarpa <rocco.arpa@optimad.it>
2023-12-19 12:58:39 +0100

    external: fix for findPETSC in Win/MSVC context

--- external/PETSc/cmake/CorrectWindowsPaths.cmake
+++ external/PETSc/cmake/CorrectWindowsPaths.cmake
@@ -1,14 +1,18 @@
 # CorrectWindowsPaths - this module defines one macro
 #
 # CONVERT_CYGWIN_PATH( PATH )
-#  This uses the command cygpath (provided by cygwin) to convert
+#  This uses the command cygpath (provided by cygwin or msys2) to convert
 #  unix-style paths into paths useable by cmake on windows
+#
+# BEWARE WIN32 enviroments will need the variable PETSC_CYGPATH_EXECUTABLE 
+# (containing the path to cygpath.exe command coming with Cygwin or Msys2)  
+# to enable path conversion from PETSc Unix paths to win compliant paths. 
 
 macro (CONVERT_CYGWIN_PATH _path)
-  if (WIN32)
-    EXECUTE_PROCESS(COMMAND cygpath.exe -m ${${_path}}
+  if (PETSC_CYGPATH_EXECUTABLE)
+    EXECUTE_PROCESS(COMMAND ${PETSC_CYGPATH_EXECUTABLE} -m ${${_path}}
       OUTPUT_VARIABLE ${_path})
-    string (STRIP ${${_path}} ${_path})
-  endif (WIN32)
+      string (STRIP ${${_path}} ${_path})
+  endif (PETSC_CYGPATH_EXECUTABLE)
 endmacro (CONVERT_CYGWIN_PATH)
 
