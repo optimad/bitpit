b2989dadb6fa3aecce808aa62dc8d39849039895
Andrea Iob <andrea_iob@hotmail.com>
2024-05-10 22:18:27 +0200

    examples/LA: merge the examples that demonstrate the functionalities of the system solver

--- examples/LA_example_00001.cpp
+++ examples/LA_example_00001.cpp
@@ -35,54 +35,182 @@ using namespace bitpit;
 /*!
  * \example LA_example_00001.cpp
  *
- * \brief This example shows how to dump a SystemSolver.
+ * \brief This example shows how to dump and restore a SystemSolver.
  *
  * This example builds a small linear system, it dumps it to the disk,
  * it solves the linear system and it exports the solution to disk.
- * Destination of dump files is ./data/ folder and they are:
- * example_initialized_linear_system_info.dat
- * example_initialized_linear_system_A.dat.info
- * example_initialized_linear_system_A.dat
- * example_initialized_linear_system_solution.dat.info
- * example_initialized_linear_system_solution.dat
- * example_initialized_linear_system_rhs.dat.info
- * example_initialized_linear_system_rhs.dat
- * example_linear_system_solution.dat.info
- * example_linear_system_solution.dat
+ * Dump files will be saved in the current directory and they names are:
+ *   LA_example_0001_linear_system_info.dat
+ *   LA_example_0001_linear_system_A.dat.info
+ *   LA_example_0001_linear_system_A.dat
+ *   LA_example_0001_linear_system_solution.dat.info
+ *   LA_example_0001_linear_system_solution.dat
+ *   LA_example_0001_linear_system_rhs.dat.info
+ *   LA_example_0001_linear_system_rhs.dat
+ *   LA_example_0001_linear_system_solution.dat.info
+ *   LA_example_0001_linear_system_solution.dat
+ *
+ * This example contains an extension of the bare SystemSolver class
+ * adding the ability to solve using algebraic multigrid (PETSc GAMG)
+ * preconditioner. Only non-block matrices can be solved using GAMG.
+ * A check disables algebraic multigrid for block matrices with a
+ * message in the logger.
  *
  * <b>To run</b>: ./LA_example_00001.cpp \n
  */
 
 /*!
- * Run the example
+ * \class AMGSystemSolver
  *
- * Example of SystemSolver dump functionality.
+ * \brief The class AMGSystemSolver is derived form SystemSolver and it
+ * allows to solve the linear system using PETSc GAMG preconditioner.
+ * Only non-block matrices can be solved using GAMG.
+ */
+class AMGSystemSolver : public SystemSolver {
+
+public:
+    /*!
+     * Constructor.
+     *
+     * \param transpose if true the solver solves the transposed linear system.
+     * \param multigrid if true the solver uses algebraic multigrid as preconditioner.
+     * \param debug if true PETSc monitors on residual, reason and singular values are turned on.
+     */
+    AMGSystemSolver(bool transpose, bool multigrid, bool debug)
+        : SystemSolver(transpose, debug),
+          m_multigrid(multigrid)
+    {
+    }
+
+    /*!
+     * Set the algebraic multigrid preconditioner.
+     *
+     * \param multigrid if true the algebraic multigrid preconditioner is turned on.
+     */
+    void setMultigridFlag(bool multigrid)
+    {
+        m_multigrid = multigrid;
+    }
+
+protected:
+    const static PetscInt GAMG_MAX_LEVELS;
+    const static PetscReal GAMG_THRESHOLD;
+    const static PetscReal GAMG_THRESHOLD_SCALE;
+
+    bool m_multigrid;
+
+    /*!
+    * Set up the specified preconditioner using the given options.
+    *
+    * \param pc is the preconditioner to set up
+    * \param options are the options that will be used to set up the preconditioner
+    */
+    void setupPreconditioner(PC pc, const KSPOptions &options) const override
+    {
+        // Early return for non-multigrid
+        if (!m_multigrid) {
+            SystemSolver::setupPreconditioner(pc, options);
+            return;
+        }
+
+        // Set multigrid options
+        KSPGetPC(this->m_KSP, &pc);
+        PCSetType(pc, PCGAMG);
+
+        std::vector<PetscReal> thresholds(GAMG_MAX_LEVELS);
+        for (int i = 0; i < GAMG_MAX_LEVELS; ++i) {
+            thresholds[i] = GAMG_THRESHOLD;
+        }
+
+        PCGAMGSetNSmooths(pc, 0);
+#if (PETSC_VERSION_MAJOR >= 3 && PETSC_VERSION_MINOR <= 17)
+#if BITPIT_ENABLE_MPI == 1
+        if (this->isPartitioned()) {
+            PCGAMGSetSymGraph(pc, PETSC_TRUE);
+        }
+#endif
+#endif
+
+        PCGAMGSetNlevels(pc, GAMG_MAX_LEVELS);
+#if (PETSC_VERSION_MAJOR >= 3 && PETSC_VERSION_MINOR >= 16)
+        PCGAMGSetThreshold(pc, thresholds.data(), GAMG_MAX_LEVELS);
+        PCGAMGSetThresholdScale(pc, GAMG_THRESHOLD_SCALE);
+#else
+        PCGAMGSetThreshold(pc, GAMG_THRESHOLD);
+#endif
+
+        // Setup
+        PCSetUp(pc);
+
+        // Set options for the multigrid coarse level
+        KSP coarseKSP;
+        PCMGGetCoarseSolve(pc, &coarseKSP);
+
+        PC coarsePreconditioner;
+        KSPGetPC(coarseKSP, &coarsePreconditioner);
+        PCSetType(coarsePreconditioner, PCSVD);
+    }
+};
+
+const PetscInt AMGSystemSolver::GAMG_MAX_LEVELS       = 10;
+const PetscReal AMGSystemSolver::GAMG_THRESHOLD       = 0.02;
+const PetscReal AMGSystemSolver::GAMG_THRESHOLD_SCALE = 1.0;
+
+/*!
+ * Run the example that show how to dump a systemSolver.
  *
  * \param rank is the rank of the process
  * \param nProcs is the number of processes
  */
-int run(int rank, int nProcs)
+int run_dump(int rank, int nProcs)
 {
+    log::cout() << std::endl;
+    log::cout() << "Running dump example..." << std::endl;
+
     int nRows;
     int nCols;
     int nNZ;
-    if (nProcs > 1 && rank == 0) {
-        nRows = 0;
-        nCols = 0;
-        nNZ   = 0;
-    } else {
+    if (nProcs == 1) {
         nRows = 10;
         nCols = 10;
         nNZ   = 10;
+    } else if (nProcs == 2) {
+        if (rank == 0) {
+            nRows = 6;
+            nCols = 6;
+            nNZ   = 6;
+        } if (rank == 1) {
+            nRows = 4;
+            nCols = 4;
+            nNZ   = 4;
+        }
+    } else {
+        if (rank == 0) {
+            nRows = 2;
+            nCols = 2;
+            nNZ   = 2;
+        } if (rank == 1) {
+            nRows = 5;
+            nCols = 5;
+            nNZ   = 5;
+        } if (rank == 2) {
+            nRows = 3;
+            nCols = 3;
+            nNZ   = 3;
+        } else {
+            nRows = 0;
+            nCols = 0;
+            nNZ   = 0;
+        }
     }
 
-    int blockSize = 1;
+    int blockSize = 5;
 
     // Build matrix
     log::cout() << "Building matrix..." << std::endl;
 
     std::vector<long> rowPattern(1);
-    std::vector<double> rowValues(1);
+    std::vector<double> rowValues(blockSize * blockSize);
 
 #if BITPIT_ENABLE_MPI==1
     SparseMatrix matrix(MPI_COMM_WORLD, true, blockSize, nRows, nCols, nNZ);
@@ -94,12 +222,15 @@ int run(int rank, int nProcs)
 
     int rowOffset = 0;
     int colOffset = 0;
-
 #endif
 
     for (int i = 0; i < nRows; ++i) {
         rowPattern[0] = colOffset + i;
-        rowValues[0]  = 1. / (double) (rowOffset + i + 1);
+        for (int k = 0; k < blockSize; ++k) {
+            int kk = linearalgebra::linearIndexRowMajor(k, k, blockSize, blockSize);
+
+            rowValues[kk]  = 1. / (double) (blockSize * (rowOffset + i) + k + 1);
+        }
 
         matrix.addRow(rowPattern, rowValues);
     }
@@ -109,11 +240,15 @@ int run(int rank, int nProcs)
     // Build solver
     log::cout() << "Building solver..." << std::endl;
 
-    SystemSolver solver;
+    bool multigrid = false;
+    bool debug     = false;
+    bool transpose = false;
+    AMGSystemSolver solver(transpose, multigrid, debug);
+
     solver.assembly(matrix);
 
     double *rhs = solver.getRHSRawPtr();
-    for (int i = 0; i < nCols; ++i) {
+    for (int i = 0; i < blockSize * nCols; ++i) {
         rhs[i] = 1.;
     }
     solver.restoreRHSRawPtr(rhs);
@@ -124,10 +259,15 @@ int run(int rank, int nProcs)
     }
     solver.restoreSolutionRawPtr(initialSolution);
 
+    // Export system
+    solver.exportMatrix("LA_example_0001_matrix.txt", SystemSolver::FILE_ASCII);
+    solver.exportRHS("LA_example_0001_rhs.txt", SystemSolver::FILE_ASCII);
+    solver.exportSolution("LA_example_0001_solution.txt", SystemSolver::FILE_ASCII);
+
     // Dump system
     log::cout() << "Dumping initialized linear system..." << std::endl;
 
-    solver.dumpSystem("bitpit linear system", "data/", "example_initialized_linear_system_");
+    solver.dumpSystem("bitpit linear system", ".", "LA_example_0001_linear_system_");
 
     // Set KSP
     KSPOptions &options = solver.getKSPOptions();
@@ -139,10 +279,151 @@ int run(int rank, int nProcs)
     log::cout() << "Solve linear system..." << std::endl;
     solver.solve();
 
+    const KSPStatus &status = solver.getKSPStatus();
+    log::cout() << "Reason: " << status.convergence << " in its: " << status.its << std::endl;
+    log::cout() << "Linear system solved." << std::endl;
+
     // Export solution
     log::cout() << "Export solution..." << std::endl;
-    std::string solutionPath = std::string("./data/") + std::string("example_linear_system_solution.dat");
-    solver.exportSolution(solutionPath, bitpit::SystemSolver::FILE_BINARY);
+    solver.exportSolution("LA_example_0001_linear_system_solution.dat", bitpit::SystemSolver::FILE_BINARY);
+
+    // Compare computed solution with expected one
+    //
+    // Set comparison tolerance to the linear system relative tolerance
+    log::cout() << "Comparing solutions..." << std::endl;
+
+    const double TOLERANCE = options.rtol;
+    log::cout() << "    Tolerance = " << options.rtol << std::endl;
+
+    std::size_t nRowElements = solver.getRowElementCount();
+
+    bool isSolutionCorrect = true;
+    const double *solution = solver.getSolutionRawReadPtr();
+    for (std::size_t i = 0; i < nRowElements; ++i) {
+        std::size_t globalDOF = rowOffset * blockSize + i;
+
+        // Check for absolute tolerance only
+        double expectedSolution = globalDOF + 1;
+        if (!utils::DoubleFloatingEqual()(expectedSolution - solution[i], 0.0, TOLERANCE, TOLERANCE)) {
+            std::stringstream message;
+            message << "Solutions do not match for global DOF #" << globalDOF << ".";
+            message << " Expected solution is " << expectedSolution;
+            message << ", current solution is " << solution[i] << ".";
+            log::cout() << message.str() << std::endl;
+            isSolutionCorrect = false;
+        }
+    }
+    solver.restoreSolutionRawReadPtr(solution);
+    if (isSolutionCorrect) {
+        log::cout() << "Solutions match." << std::endl;
+    }
+
+    return 0;
+}
+
+/*!
+ * Run the example that show how to restore a systemSolver.
+ *
+ * \param rank is the rank of the process
+ * \param nProcs is the number of processes
+ */
+int run_restore(int rank, int nProcs)
+{
+    BITPIT_UNUSED(rank);
+    BITPIT_UNUSED(nProcs);
+
+    log::cout() << std::endl;
+    log::cout() << "Running restore example..." << std::endl;
+
+    // Restore solver with initial guess
+    log::cout() << "Restoring linear system ..." << std::endl;
+    bool multigrid = false;
+    bool debug     = false;
+    bool transpose = false;
+    AMGSystemSolver solver(transpose, multigrid, debug);
+
+#if BITPIT_ENABLE_MPI==1
+    solver.restoreSystem(MPI_COMM_WORLD, ".", "LA_example_0001_linear_system_");
+#else
+    solver.restoreSystem(".", "LA_example_0001_linear_system_");
+#endif
+    log::cout() << "Linear system restored." << std::endl;
+
+    // Export system
+    solver.exportMatrix("LA_example_0001_restored_matrix.txt", SystemSolver::FILE_ASCII);
+    solver.exportRHS("LA_example_0001_restored_rhs.txt", SystemSolver::FILE_ASCII);
+    solver.exportSolution("LA_example_0001_restored_solution.txt", SystemSolver::FILE_ASCII);
+
+    // Check if multigrid is possible. Multigrid is not possible on BAIJ matrices
+    if (multigrid) {
+        log::cout() << "Enable multigrid preconditioner..." << std::endl;
+        int blockSize        = solver.getBlockSize();
+        bool enableMultigrid = (blockSize == 1);
+        solver.setMultigridFlag(enableMultigrid);
+        if (enableMultigrid) {
+            log::cout() << "Multigrid preconditioner enabled..." << std::endl;
+        } else {
+            log::cout() << "The matrix is a block matrix, no multigrid preconditioning is possible." << std::endl;
+        }
+    }
+
+    // Set KSP options
+    KSPOptions &options = solver.getKSPOptions();
+    options.atol        = 1e-50;
+    options.rtol        = 1e-7;
+    options.maxits      = 100;
+
+    // Solve linear system
+    log::cout() << "Solving linear system..." << std::endl;
+    solver.solve();
+
+    const KSPStatus &status = solver.getKSPStatus();
+    log::cout() << "Reason: " << status.convergence << " in its: " << status.its << std::endl;
+    log::cout() << "Linear system solved." << std::endl;
+
+    // Turn solution comparison on
+    bool compareSolutions = true;
+    if (compareSolutions) {
+        // Store expected solution for comparison
+        log::cout() << "Storing computed solution for comparison..." << std::endl;
+        std::size_t nRowElements = solver.getRowElementCount();
+        std::vector<double> expectedSolution(nRowElements, 0.0);
+        double *computedSolution = solver.getSolutionRawPtr();
+        for (std::size_t i = 0; i < nRowElements; ++i) {
+            expectedSolution[i] = computedSolution[i];
+        }
+        solver.restoreSolutionRawPtr(computedSolution);
+
+        // Import solution with solution from outside
+        log::cout() << "Restoring solution..." << std::endl;
+        solver.importSolution("LA_example_0001_linear_system_solution.dat");
+        log::cout() << "Solution restored." << std::endl;
+
+        // Compare computed solution with restored one
+        //
+        // Set comparison tolerance to the linear system relative tolerance
+        log::cout() << "Comparing solutions..." << std::endl;
+
+        const double TOLERANCE = options.rtol;
+        log::cout() << "    Tolerance = " << options.rtol << std::endl;
+
+        bool isSolutionCorrect = true;
+        const double *solution = solver.getSolutionRawReadPtr();
+        for (std::size_t i = 0; i < nRowElements; ++i) {
+            if (!utils::DoubleFloatingEqual()(expectedSolution[i] - solution[i], 0.0, TOLERANCE, TOLERANCE)) {
+                std::stringstream message;
+                message << "Solutions do not match for local DOF #" << i << ".";
+                message << " Expected solution is " << expectedSolution[i];
+                message << ", current solution is " << solution[i] << ".";
+                log::cout() << message.str() << std::endl;
+                isSolutionCorrect = false;
+            }
+        }
+        solver.restoreSolutionRawReadPtr(solution);
+        if (isSolutionCorrect) {
+            log::cout() << "Solutions match." << std::endl;
+        }
+    }
 
     return 0;
 }
@@ -173,11 +454,16 @@ int main(int argc, char *argv[])
     log::cout().setDefaultVisibility(log::GLOBAL);
 
     // Run the subtests
-    log::cout() << "Build, dump (initialization), solve linear system and dump the solution" << std::endl;
+    log::cout() << "Build, dump, solve and restore linear system" << std::endl;
 
     int status;
     try {
-        status = run(rank, nProcs);
+        status = run_dump(rank, nProcs);
+        if (status != 0) {
+            return status;
+        }
+
+        status = run_restore(rank, nProcs);
         if (status != 0) {
             return status;
         }
